224ee27060366f8b888e6e889037ee52c669257e
Mason Larobina <mason.larobina@gmail.com>
2010-08-06 21:38:24 +0800

    Move to a gtklabel constructed tablist rather than notebook tabs

--- rc.lua
+++ rc.lua
@@ -15,24 +15,33 @@ function window()   return widget{type="window"}   end
 function entry()    return widget{type="entry"}    end
 
 -- Variable definitions
-HOMEPAGE    = "http://github.com/mason-larobina/luakit"
+HOMEPAGE    = "http://luakit.org/"
+--HOMEPAGE  = "http://github.com/mason-larobina/luakit"
 SCROLL_STEP = 20
 
 -- Luakit theme
 theme = theme or {
-    -- Generic settings
+    -- Default settings
     font = "monospace normal 9",
     fg   = "#fff",
     bg   = "#000",
 
-    -- Slightly specific settings
+    -- General settings
     statusbar_fg = "#fff",
     statusbar_bg = "#000",
     inputbar_fg  = "#000",
     inputbar_bg  = "#fff",
 
     -- Specific settings
-    loaded_fg    = "#888",
+    loaded_fg            = "#33AADD",
+    tablabel_fg          = "#999",
+    tablabel_bg          = "#111",
+    selected_tablabel_fg = "#fff",
+    selected_tablabel_bg = "#000",
+
+    -- Enforce a minimum tab width of 30 characters to prevent longer tab
+    -- titles overshadowing small tab titles when things get crowded.
+    tablabel_format      = "%-30s",
 }
 
 widget.add_signal("new", function(wi)
@@ -85,47 +94,21 @@ mode_binds = {
     insert = { },
 }
 
-function apply_theme(w)
-    local s, i, fg, bg, font = w.sbar, w.ibar, theme.fg, theme.bg, theme.font
-    -- Set foregrounds
-    for wi, v in pairs({
-        [s.l.uri]    = theme.uri_fg    or theme.statusbar_fg or fg,
-        [s.l.loaded] = theme.loaded_fg or theme.statusbar_fg or fg,
-        [s.r.buf]    = theme.buf_fg    or theme.statusbar_fg or fg,
-        [s.r.tabi]   = theme.tabi_fg   or theme.statusbar_fg or fg,
-        [s.r.scroll] = theme.scroll_fg or theme.statusbar_fg or fg,
-        [i.prompt]   = theme.prompt_fg or theme.inputbar_fg  or fg,
-        [i.input]    = theme.input_fg  or theme.inputbar_fg  or fg,
-    }) do wi.fg = v end
-
-    -- Set backgrounds
-    for wi, v in pairs({
-        [s.l.ebox]   = theme.statusbar_bg or bg,
-        [s.r.ebox]   = theme.statusbar_bg or bg,
-        [s.ebox]     = theme.statusbar_bg or bg,
-        [i.ebox]     = theme.inputbar_bg  or bg,
-        [i.input]    = theme.input_bg     or theme.inputbar_bg or bg,
-    }) do wi.bg = v end
-
-    -- Set fonts
-    for wi, v in pairs({
-        [s.l.uri]    = theme.uri_font    or theme.statusbar_font or font,
-        [s.l.loaded] = theme.loaded_font or theme.statusbar_font or font,
-        [s.r.buf]    = theme.buf_font    or theme.statusbar_font or font,
-        [s.r.tabi]   = theme.tabi_font   or theme.statusbar_font or font,
-        [s.r.scroll] = theme.scroll_font or theme.statusbar_font or font,
-        [i.prompt]   = theme.prompt_font or theme.inputbar_font or font,
-        [i.input]    = theme.input_font  or theme.inputbar_font or font,
-    }) do wi.font = v end
-end
 
 -- Build and pack window widgets
 function build_window()
     -- Create a table for widgets and state variables for a window
     local w = {
         win    = window(),
+        ebox   = eventbox(),
         layout = vbox(),
         tabs   = notebook(),
+        -- Tab bar widgets
+        tbar = {
+            layout = hbox(),
+            ebox   = eventbox(),
+            titles = { },
+        },
         -- Status bar widgets
         sbar = {
             layout = hbox(),
@@ -158,7 +141,15 @@ function build_window()
     }
 
     -- Assemble window
-    w.win:set_child(w.layout)
+    w.ebox:set_child(w.layout)
+    w.win:set_child(w.ebox)
+
+    -- Pack tab bar
+    local t = w.tbar
+    t.ebox:set_child(t.layout, false, false, 0)
+    w.layout:pack_start(t.ebox, false, false, 0)
+
+    -- Pack notebook
     w.layout:pack_start(w.tabs, true, true, 0)
 
     -- Pack left-aligned statusbar elements
@@ -189,14 +180,11 @@ function build_window()
     i.ebox:set_child(i.layout)
     w.layout:pack_start(i.ebox,    false, false, 0)
 
-    apply_theme(w)
-
     -- Other settings
     i.input.show_frame = false
+    w.tabs.show_tabs = false
     l.loaded:hide()
     l.uri.selectable = true
-    r.scroll.text = "All"
-    r.tabi.text = "[0/0]"
 
     return w
 end
@@ -205,6 +193,7 @@ function attach_window_signals(w)
     -- Attach notebook widget signals
     w.tabs:add_signal("page-added", function(nbook, view, idx)
         w:update_tab_count(idx)
+        w:update_tab_labels()
     end)
 
     w.tabs:add_signal("switch-page", function(nbook, view, idx)
@@ -212,7 +201,7 @@ function attach_window_signals(w)
         w:update_win_title(view)
         w:update_uri(view)
         w:update_progress(view)
-        w:update_tab_hide()
+        w:update_tab_labels(idx)
     end)
 
     -- Attach window widget signals
@@ -258,14 +247,14 @@ end
 -- Attach signal handlers to a new tab's webview
 function attach_webview_signals(w, view)
     view:add_signal("title-changed", function (v)
-        w:update_tab_title(v)
+        w:update_tab_labels()
         if w:is_current(v) then
             w:update_win_title(v)
         end
     end)
 
     view:add_signal("property::uri", function(v)
-        w:update_tab_title(v)
+        w:update_tab_labels()
         if w:is_current(v) then
             w:update_uri(v)
         end
@@ -299,7 +288,6 @@ function attach_webview_signals(w, view)
     end)
 end
 
-
 -- Parses scroll amounts of the form:
 --   Relative: "+20%", "-20%", "+20px", "-20px"
 --   Absolute: 20, "20%", "20px"
@@ -331,6 +319,11 @@ window_helpers = {
     is_mode     = function(w, name)    return name == w:get_mode()                        end,
     is_any_mode = function(w, t, name) return util.table.hasitem(t, name or w:get_mode()) end,
 
+    get_tab_title = function (w, view)
+        if not view then view = w:get_current() end
+        return view:get_prop("title") or view.uri or "(Untitled)"
+    end,
+
     navigate = function(w, uri, view)
         (view or w:get_current()).uri = uri
     end,
@@ -338,9 +331,9 @@ window_helpers = {
     new_tab = function(w, uri)
         local view = webview()
         w.tabs:append(view)
-        w:update_tab_hide()
         attach_webview_signals(w, view)
         if uri then view.uri = uri end
+        view.show_scrollbars = false
         w:update_tab_count()
     end,
 
@@ -361,6 +354,7 @@ window_helpers = {
         end
         view:set_scroll_vert(value)
     end,
+
     scroll_horiz = function(w, value)
         if not view then view = w:get_current() end
         local cur, max = view:get_scroll_horiz()
@@ -369,7 +363,6 @@ window_helpers = {
         end
         view:set_scroll_horiz(value)
     end,
-
 
     -- Tab traversing functions
     next_tab = function(w, n)
@@ -393,10 +386,6 @@ window_helpers = {
     -- GUI content update functions
     update_tab_count = function (w, i, t)
         w.sbar.r.tabi.text = string.format("[%d/%d]", i or w.tabs:current(), t or w.tabs:count())
-    end,
-
-    update_tab_title = function (w, view, title)
-        w.tabs:set_title(view, title or view:get_prop("title") or view.uri or "(Untitled)")
     end,
 
     update_win_title = function (w, view)
@@ -434,11 +423,6 @@ window_helpers = {
         w.sbar.r.scroll.text = val
     end,
 
-    update_tab_hide = function (w, n)
-        if not n then n = w.tabs:count() end
-        w.tabs.show_tabs = (n ~= 1)
-    end,
-
     update_buf = function (w)
         if w.buffer then
             w.sbar.r.buf.text = string.format(" %-3s", w.buffer)
@@ -458,6 +442,106 @@ window_helpers = {
         w.buffer = nil
         w:update_buf()
     end,
+
+    -- Tab label functions
+    make_tab_label = function (w, pos)
+        local t = {
+            label  = label(),
+            sep    = label(),
+            ebox   = eventbox(),
+            layout = hbox(),
+        }
+        t.label.font = theme.tablabel_font or theme.font
+        t.layout:pack_start(t.label, true,  true, 0)
+        t.layout:pack_start(t.sep,   false,  false, 0)
+        t.ebox:set_child(t.layout)
+        t.ebox:add_signal("clicked", function(e) w.tabs:switch(pos) end)
+        return t
+    end,
+
+    destroy_tab_label = function(w, t)
+        if not t then t = table.remove(w.tbar.titles) end
+        for _, wi in pairs(t) do
+            wi:destroy()
+        end
+    end,
+
+    update_tab_labels = function (w, current)
+        local tb = w.tbar
+        local count, current = w.tabs:count(), current or w.tabs:current()
+
+        if count ~= #tb.titles then
+            -- Grow the number of labels
+            while count > #tb.titles do
+                local t = w:make_tab_label(#tb.titles + 1)
+                tb.layout:pack_start(t.ebox, true, true,  0)
+                table.insert(tb.titles, t)
+            end
+            -- Prune number of labels
+            while count < #tb.titles do
+                w:destroy_tab_label()
+            end
+        end
+
+        if count ~= 0 then
+            for i = 1, count do
+                local t = tb.titles[i]
+                local title = " " ..i.. " "..w:get_tab_title(w.tabs:atindex(i))
+                t.label.text = string.format(theme.tablabel_format or "%s", title)
+                w:apply_tablabel_theme(t, i == current)
+            end
+        end
+
+    end,
+
+    -- Theme functions
+    apply_tablabel_theme = function (w, t, selected, atheme)
+        local theme = atheme or theme
+        if selected then
+            t.label.fg = theme.selected_tablabel_fg or theme.tablabel_fg or theme.fg
+            t.ebox.bg  = theme.selected_tablabel_bg or theme.tablabel_bg or theme.bg
+        else
+            t.label.fg = theme.tablabel_fg or theme.fg
+            t.ebox.bg  = theme.tablabel_bg or theme.bg
+        end
+    end,
+
+    apply_window_theme = function (w, atheme)
+        local theme        = atheme or theme
+        local s, i, t      = w.sbar, w.ibar, w.tbar
+        local fg, bg, font = theme.fg, theme.bg, theme.font
+
+        -- Set foregrounds
+        for wi, v in pairs({
+            [s.l.uri]    = theme.uri_fg    or theme.statusbar_fg or fg,
+            [s.l.loaded] = theme.loaded_fg or theme.statusbar_fg or fg,
+            [s.r.buf]    = theme.buf_fg    or theme.statusbar_fg or fg,
+            [s.r.tabi]   = theme.tabi_fg   or theme.statusbar_fg or fg,
+            [s.r.scroll] = theme.scroll_fg or theme.statusbar_fg or fg,
+            [i.prompt]   = theme.prompt_fg or theme.inputbar_fg  or fg,
+            [i.input]    = theme.input_fg  or theme.inputbar_fg  or fg,
+        }) do wi.fg = v end
+
+        -- Set backgrounds
+        for wi, v in pairs({
+            [s.l.ebox]   = theme.statusbar_bg or bg,
+            [s.r.ebox]   = theme.statusbar_bg or bg,
+            [s.ebox]     = theme.statusbar_bg or bg,
+            [i.ebox]     = theme.inputbar_bg  or bg,
+            [i.input]    = theme.input_bg     or theme.inputbar_bg or bg,
+        }) do wi.bg = v end
+
+        -- Set fonts
+        for wi, v in pairs({
+            [s.l.uri]    = theme.uri_font    or theme.statusbar_font or font,
+            [s.l.loaded] = theme.loaded_font or theme.statusbar_font or font,
+            [s.r.buf]    = theme.buf_font    or theme.statusbar_font or font,
+            [s.r.tabi]   = theme.tabi_font   or theme.statusbar_font or font,
+            [s.r.scroll] = theme.scroll_font or theme.statusbar_font or font,
+            [i.prompt]   = theme.prompt_font or theme.inputbar_font or font,
+            [i.input]    = theme.input_font  or theme.inputbar_font or font,
+        }) do wi.font = v end
+    end,
 }
 
 -- Create new window
@@ -468,6 +552,9 @@ function new_window(uris)
     for k, v in pairs(window_helpers) do w[k] = v end
 
     attach_window_signals(w)
+
+    -- Apply window theme
+    w:apply_window_theme()
 
     -- Populate notebook with tabs
     for _, uri in ipairs(uris or {}) do
