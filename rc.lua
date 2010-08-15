d53d137f35bbe52dd8a7cd572050b70c8cea7f1e
karottenreibe <karottenreibe@gmail.com>
2010-08-15 12:34:22 +0200

    now displaying basename of downloaded file instead of uri in downloadbar

--- rc.lua
+++ rc.lua
@@ -932,7 +932,8 @@ window_helpers = {
     end,
 
     update_download_widget = function(w, wi, d)
-        wi.text = string.format("%s: %s (%s%%)", d.status, d.uri, d.progress * 100)
+        _,_,basename = string.find(d.destination, ".*/([^/]*)")
+        wi.text = string.format("%s: %s (%s%%)", d.status, basename, d.progress * 100)
     end,
 
     -- Updates the widgets in the download bar.
