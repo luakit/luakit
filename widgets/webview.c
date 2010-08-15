a1ef8e5681dffe80d87e095f0c8cc6c01b3ca5e5
karottenreibe <karottenreibe@gmail.com>
2010-08-15 15:09:50 +0200

    now all unknown mime types are handled by downloading the files.

--- widgets/webview.c
+++ widgets/webview.c
@@ -313,6 +313,26 @@ link_hover_cb(WebKitWebView *view, const char *t, const gchar *link, widget_t *w
     lua_pop(L, 1);
 }
 
+/* Starts a download for all MIME types the WebView can't handle
+ * itself.
+ */
+static gboolean
+mime_type_decision_cb(WebKitWebView *v, WebKitWebFrame *f,
+        WebKitNetworkRequest *r, gchar *mime_type,
+        WebKitWebPolicyDecision *p, widget_t *w)
+{
+    (void) f;
+    (void) r;
+    (void) w;
+
+    if(!webkit_web_view_can_show_mime_type(v, mime_type)) {
+        webkit_web_policy_decision_download(p);
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
 /* Raises the "navigation-request" signal on a webkit navigation policy
  * decision request. The default action is to load the requested uri.
  *
@@ -811,6 +831,7 @@ widget_webview(widget_t *w)
       "signal::parent-set",                           (GCallback)parent_set_cb,          w,
       "signal::title-changed",                        (GCallback)title_changed_cb,       w,
       "signal::download-requested",                   (GCallback)download_requested_cb,  w,
+      "signal::mime-type-policy-decision-requested",  (GCallback)mime_type_decision_cb,  w,
       NULL);
 
     /* setup */
