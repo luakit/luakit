e4c7369bbef9977569662703154214cbe1f96d4e
karottenreibe <karottenreibe@gmail.com>
2010-08-31 09:12:03 +0200

    added some lfs functionality

--- lib/userscripts.lua
+++ lib/userscripts.lua
@@ -2,6 +2,8 @@ local setfenv = setfenv
 local print = print
 local util = require("lousy.util")
 local env = getfenv(getfenv)
+local lfs = require("lfs")
+local loadfile = loadfile
 
 --- Evaluates userscripts with a simple standard API to
 -- deal wih common userscript problems.
@@ -17,5 +19,14 @@ dir = util.find_data("/scripts")
 
 --- Loads all userscripts from the <code>dir</code>.
 function init()
+    for f in lfs.dir(dir) do
+        fun, msg = loadfile(dir .. "/" .. f)
+        if fun then
+            setfenv(fun, env)
+            fun()
+        else
+            io.stderr:write(msg .. "\n")
+        end
+    end
 end
 
