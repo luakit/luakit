7bf6302c1df4de0196789431775215adfc175593
Mason Larobina <mason.larobina@gmail.com>
2010-09-14 22:33:48 +0800

    Clean up useragent generation.

--- config/globals.lua
+++ config/globals.lua
@@ -11,13 +11,11 @@ globals = {
 }
 
 -- Make useragent
-local rv, out, err = luakit.spawn_sync("uname -sm")
-local webkit_version = string.format("WebKitGTK+/%d.%d.%d", luakit.webkit_major_version,
-    luakit.webkit_minor_version, luakit.webkit_micro_version)
-local luakit_version = string.format("luakit/%s", luakit.version)
-local webkit_ua_major_version = luakit.webkit_user_agent_major_version
-local webkit_ua_minor_version = luakit.webkit_user_agent_minor_version
-globals.useragent = string.format("Mozilla/5.0 (%s) AppleWebKit/%s.%s+ %s %s", string.match(out, "([^\n]*)"), webkit_ua_major_version, webkit_ua_minor_version, webkit_version, luakit_version)
+local arch = string.match(({luakit.spawn_sync("uname -sm")})[2], "([^\n]*)")
+local lkv  = string.format("luakit/%s", luakit.version)
+local wkv  = string.format("WebKitGTK+/%d.%d.%d", luakit.webkit_major_version, luakit.webkit_minor_version, luakit.webkit_micro_version)
+local awkv = string.format("AppleWebKit/%s.%s+", luakit.webkit_user_agent_major_version, luakit.webkit_user_agent_minor_version)
+globals.useragent = string.format("Mozilla/5.0 (%s) %s %s %s", arch, awkv, wkv, lkv)
 
 -- Search common locations for a ca file which is used for ssl connection validation.
 local ca_files = {luakit.data_dir .. "/ca-certificates.crt",
