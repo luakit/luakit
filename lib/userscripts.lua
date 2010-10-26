038bed08c20df6f6547a7fc8901e48bfca1bed5c
Mason Larobina <mason.larobina@gmail.com>
2010-10-26 13:18:48 +0800

    Cleaned up lib/userscripts.lua, removed lua scripts for now

--- lib/userscripts.lua
+++ lib/userscripts.lua
@@ -1,123 +1,126 @@
+---------------------------------------------------------
+-- Userscript support for luakit                       --
+-- (C) 2010 Fabian Streitel <karottenreibe@gmail.com>  --
+-- (C) 2010 Mason Larobina  <mason.larobina@gmail.com> --
+---------------------------------------------------------
+
+-- Grab environment we need
+local io = io
+local ipairs = ipairs
+local os = os
+local pairs = pairs
+local setmetatable = setmetatable
 local string = string
+local table = table
 local webview = webview
 local util = require("lousy.util")
-local env = getfenv(getfenv)
 local lfs = require("lfs")
-local loadfile = loadfile
-local table = table
-local pairs = pairs
-local io = io
-local os = os
-local type = type
-local luakit = luakit
+local capi = { luakit = luakit }
+
+local warn = function (fmt, ...)
+    io.stderr:write(string.format("(userscripts.lua): "..fmt.."\n", ...))
+end
 
 --- Evaluates and manages userscripts.
--- A userscript is either
--- a)   A lua script that returns a table representing a userscript.
---      The table needs to contain the following keys:
---      *   include     An array of patterns that match URI's, for
---                      which the script should be loaded.
---      *   exclude     An array of patterns that match URI's, for
---                      which the script should not be loaded.
---      *   script      The script itself. It will get the current window
---                      and webview as it's parameters.
---      Lua userscripts must end in <code>.user.lua</code>
--- b)   A traditional userscript.
---      JavaScript userscripts must end in <code>.user.js</code>
+-- JavaScript userscripts must end in <code>.user.js</code>
 module("userscripts")
 
 --- Stores all the scripts.
 local scripts = {}
 
 --- The directory, in which to search for userscripts.
--- By default, this is $XDG_DATA_HOME/scripts
-dir = luakit.data_dir .. "/scripts"
+-- By default, this is $XDG_DATA_HOME/luakit/scripts
+dir = capi.luakit.data_dir .. "/scripts"
 
---- Loads a lua userscript.
-local function load_lua(file)
-    fun, msg = loadfile(file)
-    if fun then
-        -- get metadata
-        script = fun()
-        script.file = file
-        table.insert(scripts, script)
-    else
-        io.stderr:write(msg .. "\n")
-    end
+-- Userscript class methods
+local prototype = {
+    -- Run the userscript on the given webview widget
+    run = function (s, view)
+        view:eval_js(s.js, string.format("(userscript:%s)", s.file))
+    end,
+    -- Check if the given uri matches the userscripts include/exclude patterns
+    match = function (s, uri)
+        local matches = false
+        for _, p in ipairs(s.include) do
+            if string.match(uri, p) then
+                matches = true
+                break
+            end
+        end
+        if matches then
+            for _, p in ipairs(s.exclude) do
+                if string.match(uri, p) then return false end
+            end
+        end
+        return matches
+    end,
+}
+
+-- Parse and convert a simple glob matching pattern in the `@include`,
+-- `@exclude` or `@match` userscript header options into an RE.
+local function parse_pattern(pat)
+    pat = string.gsub(string.gsub(pat, "[%^%$%(%)%%%.%[%]%+%-%?]", "%%%1"), "*", ".*")
+    return '^' .. pat .. '$'
 end
 
---- Extracts and converts all URI patterns of the given type (include, exclude)
--- from the userscript pattern syntax to lua patterns.
-local function all_matches(str, typ)
-    local pat = "//%s*@" .. typ .. "%s*([^\r\n]*)"
-    local arr = {}
-    for p in string.gmatch(str, pat) do
-        -- escape [%.$?^] and convert * into .*
-        p = string.gsub(string.gsub(p, "[%%.?$^]", "%%%1"), "*", ".*")
-        --p = string.format("^%s$", p)
-        table.insert(arr, p)
+local function parse_header(header, file)
+    local ret = { file = file, include = {}, exclude = {} }
+    for i, line in ipairs(util.string.split(header, "\n")) do
+        -- Parse `// @key value` line in header.
+        local key, val = string.match(line, "^// @([%w%-]+)%s+(.+)$")
+        if key then
+            val = util.string.strip(val or "")
+            -- Populate header table
+            if key == "name" or key == "description" or key == "version" then
+                -- Only grab the first of its kind
+                if not ret[key] then ret[key] = val end
+            elseif key == "include" or key == "exclude" then
+                table.insert(ret[key], parse_pattern(val))
+            elseif key == "run-at" and val == "document-start" then
+                ret.on_start = true
+            end
+        else
+            warn("Invalid line in header: %s:%d:%s", file, i, line)
+        end
     end
-    return arr
+    return ret
 end
 
 --- Loads a js userscript.
 local function load_js(file)
+    -- Open script
     local f = io.open(file, "r")
     local js = f:read("*all")
     f:close()
 
-    local header = string.match(js, "//%s*==UserScript==.*\n//%s*==/UserScript==")
+    -- Inspect userscript header
+    local header = string.match(js, "^//%s*==UserScript==%s*\n(.*)\n//%s*==/UserScript==")
     if header then
-        local include = all_matches(header, "include")
-        local exclude = all_matches(header, "exclude")
-        local fun = function(w, v) v:eval_js(js, string.format("(userscript:%s)", file)) end
-        script = {name=name,include=include,exclude=exclude,script=fun,file=file}
-        table.insert(scripts, script)
+        local script = parse_header(header, file)
+        script.js = js
+        scripts[file] = setmetatable(script, { __index = prototype })
+    else
+        warn("Invalid userscript header in file: %s", file)
     end
 end
 
 --- Loads all userscripts from the <code>userscripts.dir</code>.
-local function init()
-    if os.exists(dir) then
-        for f in lfs.dir(dir) do
-            if string.match(f, "%.user%.lua$") then
-                load_lua(dir .. "/" .. f)
-            elseif string.match(f, "%.user%.js$") then
-                load_js(dir .. "/" .. f)
-            end
+local function load_all()
+    if not os.exists(dir) then return end
+    for file in lfs.dir(dir) do
+        if string.match(file, "%.user%.js$") then
+            load_js(dir .. "/" .. file)
         end
     end
 end
 
---- Tests if the given uri is matched by the given script.
-local function included(s, uri)
-    local included = false
-    if s.include and type(s.include) == "table" then
-        for _,i in pairs(s.include) do
-            if i and string.match(uri, i) then
-                included = true
-                break
+-- Invoke all userscripts for the current uri
+local function invoke(view, uri, on_start)
+    for _, script in pairs(scripts) do
+        if on_start == script.on_start then
+            if script:match(uri) then
+                script:run(view)
             end
-        end
-    end
-    if s.exclude and type(s.exclude) == "table" then
-        for _,i in pairs(s.exclude) do
-            if i and string.match(uri, i) then
-                included = false
-                break
-            end
-        end
-    end
-    return included
-end
-
---- Invokes all userscripts for the given uri, passing them the given
--- window and view.
-local function invoke(w, v, uri)
-    for _,s in pairs(scripts) do
-        if included(s, uri) then
-            local fun = s.script and type(s.script) == "function" and s.script
-            if fun then fun(w, v) end
         end
     end
 end
@@ -125,12 +128,14 @@ end
 --- Hook on the webview's load-status signal to invoke the userscripts.
 webview.init_funcs.userscripts = function (view, w)
     view:add_signal("load-status", function (v, status)
-        if status ~= "finished" then return end
         local uri = v.uri or "about:blank"
-        invoke(w, v, uri)
+        if status == "first-visual" then
+            invoke(view, uri, true)
+        elseif status == "finished" then
+            invoke(view, uri)
+        end
     end)
 end
 
 -- Initialize the userscripts
-init()
-
+load_all()
