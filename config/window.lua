dbd3602fa2b4cb3fc70a8b1737c9735a594ad6fc
karottenreibe <karottenreibe@gmail.com>
2010-12-05 20:00:31 +0100

    added much more sophisticated domain matching algorithm this should reduce false positives in search_open

--- config/window.lua
+++ config/window.lua
@@ -654,9 +654,18 @@ window.methods = {
     search_open = function (w, arg)
         if not arg then return "about:blank" end
         args = lousy.util.string.split(lousy.util.string.strip(arg))
-        -- Detect localhost, scheme:// or "." in string
-        if #args == 1 and (string.match(args[1], "%.") or string.match(args[1], "^%w+://") or string.match(args[1], "^localhost[:/]")) then
-            return args[1]
+        -- Detect localhost, scheme:// or domain-like beginning in string
+        if #args == 1  then
+            local scheme = string.match(args[1], "^%w+://")
+            local localhost = string.match(args[1], "^localhost[:/]%S*") or string.match(args[1], "^localhost$")
+            -- Extract domain from before the first colon or slash
+            local domain = string.match(args[1], "^([%w%-_%.]+)[:/]%S*") or string.match(args[1], "^([%w%-_%.]+)$")
+            -- A valid domain consists of [%w%-_%.] and has at least one dot
+            -- with at least one [%w%-_] on the left and a TLD on the right
+            -- with at least two letters
+            if scheme or localhost or (domain and string.match(domain, "^[%w%-_%.]*[%w%-_]%.%a%a[%a%.]*$")) then
+                return args[1]
+            end
         end
         -- Find search engine
         local engine = "default"
