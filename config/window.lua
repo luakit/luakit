ad497a6b4a0c939e66f5af3dc81ba1ef5b1cb0c0
karottenreibe <karottenreibe@gmail.com>
2010-12-05 20:14:42 +0100

    made code more readable

--- config/window.lua
+++ config/window.lua
@@ -656,15 +656,16 @@ window.methods = {
         args = lousy.util.string.split(lousy.util.string.strip(arg))
         -- Detect localhost, scheme:// or domain-like beginning in string
         if #args == 1  then
-            local scheme = string.match(args[1], "^%w+://")
-            local localhost = string.match(args[1], "^localhost[:/]%S*") or string.match(args[1], "^localhost$")
+            local uri = args[1]
+            local scheme = string.match(uri, "^%w+://")
+            local localhost = string.match(uri, "^localhost[:/]%S*") or string.match(uri, "^localhost$")
             -- Extract domain from before the first colon or slash
-            local domain = string.match(args[1], "^([%w%-_%.]+)[:/]%S*") or string.match(args[1], "^([%w%-_%.]+)$")
+            local domain = string.match(uri, "^([%w%-_%.]+)[:/]%S*") or string.match(uri, "^([%w%-_%.]+)$")
             -- A valid domain consists of [%w%-_%.] and has at least one dot
             -- with at least one [%w%-_] on the left and a TLD on the right
             -- with at least two letters
             if scheme or localhost or (domain and string.match(domain, "^[%w%-_%.]*[%w%-_]%.%a%a[%a%.]*$")) then
-                return args[1]
+                return uri
             end
         end
         -- Find search engine
