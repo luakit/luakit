70e148295331e6de33d60d28703315c4b87935fd
karottenreibe <karottenreibe@gmail.com>
2010-12-27 11:37:46 +0100

    now finding right window to run authentication prompt in

--- lib/auth.lua
+++ lib/auth.lua
@@ -1,4 +1,8 @@
 local print = print
+local pairs = pairs
+local ipairs = ipairs
+local window = window
+local luakit = luakit
 local lousy = require "lousy"
 
 local new_mode, add_binds, add_cmds, menu_binds = new_mode, add_binds, add_cmds, menu_binds
@@ -6,39 +10,48 @@ local new_mode, add_binds, add_cmds, menu_binds = new_mode, add_binds, add_cmds,
 module("auth")
 
 -- the authentication prompt
-local username, password
+local dat = {
+    username = nil,
+    password = nil,
+    uri = nil,
+}
 new_mode("authenticate", {
     enter = function (w)
-        username = nil
-        password = nil
-        w:set_prompt("Login:")
+        dat.username = nil
+        dat.password = nil
+        w:set_prompt("Login for " .. dat.uri .. ":")
         w:set_input("")
     end,
 
     activate = function (w)
-        if username == nil then
-            username = w.ibar.input.text
-            w:set_prompt("Password:")
+        if dat.username == nil then
+            dat.username = w.ibar.input.text
+            w:set_prompt("Password for " .. dat.uri .. ":")
             w:set_input("")
         else
-            password = w.ibar.input.text
+            dat.password = w.ibar.input.text
             w:set_mode()
         end
     end,
 
     leave = function (w)
-        if username == nil or password == nil then
-            w:authenticate()
-        else
-            w:authenticate(username, password)
+        if dat.username ~= nil and dat.password ~= nil then
+            w:authenticate(dat.username, dat.password)
         end
     end,
 })
 
 -- register authentication function
 luakit.add_signal("authenticate", function (uri)
-    -- TODO: find window
-    w:set_mode("authenticate")
-    return true
+    for _, w in pairs(window.bywidget) do
+        for _, v in ipairs(w.tabs:get_children()) do
+            if v.uri == uri then
+                dat.uri = uri
+                w:set_mode("authenticate")
+                return true
+            end
+        end
+    end
+    return false
 end)
 
