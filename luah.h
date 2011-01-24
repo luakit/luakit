be234e9dcad551c64f0c678c020559c6b4ad7c3d
Javier Rojas <jrojasr@integracionsocial.gov.co>
2011-01-24 16:56:14 -0500

    changed callback data to pass std{out,err} file descriptors
    those descriptors will be used to pass the std{out,err} to the Lua
    callback function as text.

--- luah.h
+++ luah.h
@@ -27,6 +27,12 @@
 #include "common/luaobject.h"
 #include "common/lualib.h"
 
+typedef struct proc_callback_data_t {
+    lua_State *L;
+    gint stdout_fd;
+    gint stderr_fd;
+} proc_callback_data_t;
+
 typedef enum {
     TERM_EXIT = 1,
     TERM_SIGNAL,
