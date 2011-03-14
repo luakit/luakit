90513acacd0bc23460ae923db2d7501f2d5e6e7d
Javier Rojas <jrojasr@integracionsocial.gov.co>
2011-03-14 15:45:41 -0500

    Revert "changed callback data to pass std{out,err} file descriptors"
    This reverts commit be234e9dcad551c64f0c678c020559c6b4ad7c3d.
    
    Conflicts:
    
    	luah.c

--- luah.h
+++ luah.h
@@ -27,12 +27,6 @@
 #include "common/luaobject.h"
 #include "common/lualib.h"
 
-typedef struct proc_callback_data_t {
-    lua_State *L;
-    gint stdout_fd;
-    gint stderr_fd;
-} proc_callback_data_t;
-
 typedef enum {
     TERM_EXIT = 1,
     TERM_SIGNAL,
