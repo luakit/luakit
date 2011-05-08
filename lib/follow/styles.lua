bef054aba744ee3628207aad90bdc596de37e204
karottenreibe <k@rottenrei.be>
2011-05-08 22:47:51 +0200

    added new follow style for fixed charsets with no filtering

--- lib/follow/styles.lua
+++ lib/follow/styles.lua
@@ -9,11 +9,13 @@ local tostring = tostring
 
 --- Contains different styles for following.
 -- A style is a function that returns a hash with the following entries:
+--
 -- <ul>
 -- <li> <code>make_labels</code>
 -- <li> <code>parse_inupt</code>
 -- </ul>
 --
+-- See below for documentation of these functions
 module("follow.styles")
 
 --- Generates the labels for the hints.
@@ -25,18 +27,6 @@ module("follow.styles")
 -- @name make_labels
 
 --- Parses the user's input into a match string and an ID.
--- Can be overriden to have a different matching procedure, e.g. when
--- <code>make_labels</code> has been overridden.
---
--- <br><br><h3>Example</h3>
---
--- To only perform the following on the follow labels and not on the text
--- content of the elements, you could use
---
--- <pre>follow.parse_input = function (text)
---  <br>  return "", text
---  <br>end
--- </pre>
 --
 -- @param text The input of the user.
 -- @return A string that is used to filter the hints by their text content.
@@ -45,7 +35,26 @@ module("follow.styles")
 -- @class function
 -- @name parse_input
 
+-- Calculates the minimum number of characters needed in a hint given a
+-- charset of a certain length.
+--
+-- @param size The number of hints that need to be generated
+-- @param charset The length of the charset
+local function calculate_hint_length(size, charset)
+    local digits = 1
+    while true do
+        local max = charset ^ digits
+        if max >= size then
+            break
+        else
+            digits = digits + 1
+        end
+    end
+    return digits
+end
+
 --- Style that uses numbers for the hint labels and matches other text against the pages elements.
+--
 -- @param sort Whether to sort the hint labels
 --  <br> Not sorting can help reading labels on high link density sites.
 -- @param reverse Whether to reverse the hint labels.
@@ -53,16 +62,7 @@ module("follow.styles")
 function filtered_number_hints(sort, reverse)
     return {
         make_labels = function (size)
-            local digits = 1
-            while true do
-                local max = 10 ^ digits - 10 ^ (digits - 1)
-                if max == 9 then max = 10 end
-                if max >= size then
-                    break
-                else
-                    digits = digits + 1
-                end
-            end
+            local digits = calculate_hint_length(size, 10)
             local start = 10 ^ (digits - 1)
             if start == 1 then start = 0 end
             local labels = {}
@@ -79,7 +79,52 @@ function filtered_number_hints(sort, reverse)
 
         parse_input = function (text)
             return string.match(text, "^(.-)(%d*)$")
-        end
+        end,
+    }
+end
+
+--- Style that uses characters from a charset for the hint labels.
+--
+-- @param charset The characters to use for the labels.
+function char_hints(charset)
+    return {
+        make_labels = function (size)
+            -- calculate the number of digits to use
+            local digits = calculate_hint_length(size, #charset)
+            -- use this to track what label to generate next
+            local state = {}
+            for i = 1, digits, 1 do
+                table.insert(state, 1)
+            end
+            -- make all labels
+            local labels = {}
+            for i = 1, size, 1 do
+                -- assemble each label according to state
+                local label = ""
+                for i = 1, digits, 1 do
+                    label = string.sub(charset, state[i], state[i]) .. label
+                end
+                table.insert(labels, label)
+                -- increase the state
+                local inc
+                inc = function (digit)
+                    local s = state[digit]
+                    if not s then return end
+                    s = s + 1
+                    if s > #charset then
+                        s = 1
+                        inc(digit + 1)
+                    end
+                    state[digit] = s
+                end
+                inc(1)
+            end
+            return labels
+        end,
+
+        parse_input = function (text)
+            return "", text
+        end,
     }
 end
 
