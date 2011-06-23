8706c20f677aee51ab434d68d74685dc65b0ab95
karottenreibe <k@rottenrei.be>
2011-06-23 18:12:16 +0200

    separated javascript functions from webview.c

--- widgets/webview.c
+++ widgets/webview.c
@@ -18,7 +18,6 @@
  *
  */
 
-#include <JavaScriptCore/JavaScript.h>
 #include <webkit/webkit.h>
 #include <libsoup/soup-message.h>
 #include <math.h>
@@ -124,141 +123,7 @@ luaH_checkwebview(lua_State *L, gint udx)
     return w;
 }
 
-static JSValueRef
-webview_registered_function_callback(JSContextRef context, JSObjectRef fun,
-        JSObjectRef thisObject, size_t argumentCount,
-        const JSValueRef *arguments, JSValueRef *exception)
-{
-    (void) thisObject;
-    (void) argumentCount;
-    (void) arguments;
-
-    lua_State *L = globalconf.L;
-    gpointer ref = JSObjectGetPrivate(fun);
-    // get function
-    luaH_object_push(L, ref);
-    // call function
-    gint ret = lua_pcall(L, 0, 0, 0);
-    // handle errors
-    if (ret != 0) {
-        const gchar *exn_cstring = luaL_checkstring(L, -1);
-        lua_pop(L, 1);
-        JSStringRef exn_js_string = JSStringCreateWithUTF8CString(exn_cstring);
-        JSValueRef exn_js_value = JSValueMakeString(context, exn_js_string);
-        *exception = JSValueToObject(context, exn_js_value, NULL);
-        JSStringRelease(exn_js_string);
-    }
-    return JSValueMakeUndefined(context);
-}
-
-static void
-webview_collect_registered_function(JSObjectRef obj)
-{
-    lua_State *L = globalconf.L;
-    gpointer ref = JSObjectGetPrivate(obj);
-    luaH_object_unref(L, ref);
-}
-
-static void
-webview_register_function(WebKitWebFrame *frame, const gchar *name, gpointer ref)
-{
-    JSGlobalContextRef context = webkit_web_frame_get_global_context(frame);
-    JSStringRef js_name = JSStringCreateWithUTF8CString(name);
-    // prepare callback function
-    JSClassDefinition def = kJSClassDefinitionEmpty;
-    def.callAsFunction = webview_registered_function_callback;
-    def.className = g_strdup(name);
-    def.finalize = webview_collect_registered_function;
-    JSClassRef class = JSClassCreate(&def);
-    JSObjectRef fun = JSObjectMake(context, class, ref);
-    // register with global object
-    JSObjectRef global = JSContextGetGlobalObject(context);
-    JSObjectSetProperty(context, global, js_name, fun, kJSPropertyAttributeDontDelete | kJSPropertyAttributeReadOnly, NULL);
-    // release strings
-    JSStringRelease(js_name);
-    JSClassRelease(class);
-}
-
-static const gchar*
-webview_eval_js(WebKitWebFrame *frame, const gchar *script, const gchar *file)
-{
-    JSGlobalContextRef context;
-    JSObjectRef globalobject;
-    JSStringRef js_file;
-    JSStringRef js_script;
-    JSValueRef js_result;
-    JSValueRef js_exc = NULL;
-    JSStringRef js_result_string;
-    GString *result = g_string_new(NULL);
-    size_t js_result_size;
-
-    context = webkit_web_frame_get_global_context(frame);
-    globalobject = JSContextGetGlobalObject(context);
-
-    /* evaluate the script and get return value*/
-    js_script = JSStringCreateWithUTF8CString(script);
-    js_file = JSStringCreateWithUTF8CString(file);
-    js_result = JSEvaluateScript(context, js_script, globalobject, js_file, 0, &js_exc);
-    if (js_result && !JSValueIsUndefined(context, js_result)) {
-        js_result_string = JSValueToStringCopy(context, js_result, NULL);
-        js_result_size = JSStringGetMaximumUTF8CStringSize(js_result_string);
-
-        if (js_result_size) {
-            gchar js_result_utf8[js_result_size];
-            JSStringGetUTF8CString(js_result_string, js_result_utf8, js_result_size);
-            g_string_assign(result, js_result_utf8);
-        }
-
-        JSStringRelease(js_result_string);
-    }
-    else if (js_exc) {
-        size_t size;
-        JSStringRef prop, val;
-        JSObjectRef exc = JSValueToObject(context, js_exc, NULL);
-
-        g_printf("Exception occured while executing script:\n");
-
-        /* Print file */
-        prop = JSStringCreateWithUTF8CString("sourceURL");
-        val = JSValueToStringCopy(context, JSObjectGetProperty(context, exc, prop, NULL), NULL);
-        size = JSStringGetMaximumUTF8CStringSize(val);
-        if(size) {
-            gchar cstr[size];
-            JSStringGetUTF8CString(val, cstr, size);
-            g_printf("At %s", cstr);
-        }
-        JSStringRelease(prop);
-        JSStringRelease(val);
-
-        /* Print line */
-        prop = JSStringCreateWithUTF8CString("line");
-        val = JSValueToStringCopy(context, JSObjectGetProperty(context, exc, prop, NULL), NULL);
-        size = JSStringGetMaximumUTF8CStringSize(val);
-        if(size) {
-            gchar cstr[size];
-            JSStringGetUTF8CString(val, cstr, size);
-            g_printf(":%s: ", cstr);
-        }
-        JSStringRelease(prop);
-        JSStringRelease(val);
-
-        /* Print message */
-        val = JSValueToStringCopy(context, exc, NULL);
-        size = JSStringGetMaximumUTF8CStringSize(val);
-        if(size) {
-            gchar cstr[size];
-            JSStringGetUTF8CString(val, cstr, size);
-            g_printf("%s\n", cstr);
-        }
-        JSStringRelease(val);
-    }
-
-    /* cleanup */
-    JSStringRelease(js_script);
-    JSStringRelease(js_file);
-
-    return g_string_free(result, FALSE);
-}
+#include "widgets/webview/javascript.c"
 
 static gint
 luaH_webview_get_property(lua_State *L)
@@ -283,57 +148,6 @@ luaH_webview_load_string(lua_State *L)
     WebKitWebFrame *frame = webkit_web_view_get_main_frame(d->view);
     webkit_web_frame_load_alternate_string(frame, string, base_uri, base_uri);
     return 0;
-}
-
-static gint
-luaH_webview_register_function(lua_State *L)
-{
-    WebKitWebFrame *frame = NULL;
-    webview_data_t *d = luaH_checkwvdata(L, 1);
-    const gchar *name = luaL_checkstring(L, 2);
-
-    /* get lua callback function */
-    luaH_checkfunction(L, 3);
-    lua_pushvalue(L, 3);
-    gpointer ref = luaH_object_ref(L, -1);
-
-    /* Check if function should be registered on currently focused frame */
-    if (lua_gettop(L) >= 4 && luaH_checkboolean(L, 4))
-        frame = webkit_web_view_get_focused_frame(d->view);
-
-    /* Fall back on main frame */
-    if (!frame)
-        frame = webkit_web_view_get_main_frame(d->view);
-
-    /* register function */
-    webview_register_function(frame, name, ref);
-    return 0;
-}
-
-static gint
-luaH_webview_eval_js(lua_State *L)
-{
-    WebKitWebFrame *frame = NULL;
-    webview_data_t *d = luaH_checkwvdata(L, 1);
-    const gchar *script = luaL_checkstring(L, 2);
-    const gchar *filename = luaL_checkstring(L, 3);
-
-    /* Check if js should be run on currently focused frame */
-    if (lua_gettop(L) >= 4) {
-        if (lua_islightuserdata(L, 4))
-            frame = lua_touserdata(L, 4);
-        else if (lua_toboolean(L, 4))
-            frame = webkit_web_view_get_focused_frame(d->view);
-    }
-
-    /* Fall back on main frame */
-    if (!frame)
-        frame = webkit_web_view_get_main_frame(d->view);
-
-    /* evaluate javascript script and push return result onto lua stack */
-    const gchar *result = webview_eval_js(frame, script, filename);
-    lua_pushstring(L, result);
-    return 1;
 }
 
 static void
