3e0f3637f0afc1eff366b0be62a229b588107f7f
Mason Larobina <mason.larobina@gmail.com>
2011-06-25 17:14:00 +0800

    Beautify the paned widget
    For vpanes use vpane.{top,bottom} to get the child widgets and hpanes
    use hpane.{left,right}
    
    Also use pane:pack1(widget, { opts here }) to pack in the top/left pane
    and :pack2 to pack in the bottom/right pane.
    
    Packing options are:
     resize - should this child expand when the paned widget is resized.
     shrink - can this child be made smaller than its requisition.

--- widgets/paned.c
+++ widgets/paned.c
@@ -21,23 +21,59 @@
 #include "luah.h"
 #include "widgets/common.h"
 
-/* direct wrapper around gtk_paned_pack */
+widget_t*
+luaH_checkpaned(lua_State *L, gint udx)
+{
+    widget_t *w = luaH_checkwidget(L, udx);
+    if ((w->info->tok != L_TK_VPANED) && (w->info->tok != L_TK_HPANED))
+        luaL_argerror(L, udx, "incorrect widget type (expected paned)");
+    return w;
+}
+
 static gint
 luaH_paned_pack(lua_State *L)
 {
-    widget_t *w = luaH_checkwidget(L, 1);
-    guint n = luaL_checknumber(L, 2);
-    widget_t *child = luaH_checkwidget(L, 3);
-    gboolean resize = luaH_checkboolean(L, 4);
-    gboolean shrink = luaH_checkboolean(L, 5);
-    if (n == 1) {
-        gtk_paned_pack1(GTK_PANED(w->widget), GTK_WIDGET(child->widget),
-            resize, shrink);
-    } else {
-        gtk_paned_pack2(GTK_PANED(w->widget), GTK_WIDGET(child->widget),
-            resize, shrink);
+    widget_t *w = luaH_checkpaned(L, 1);
+    widget_t *child = luaH_checkwidget(L, 2);
+    gint top = lua_gettop(L);
+    gboolean resize = TRUE, shrink = TRUE;
+    if (top > 2 && !lua_isnil(L, 3)) {
+        luaH_checktable(L, 3);
+        if (luaH_rawfield(L, 3, "resize"))
+            resize = lua_toboolean(L, -1) ? TRUE : FALSE;
+        if (luaH_rawfield(L, 3, "shrink"))
+            shrink = lua_toboolean(L, -1) ? TRUE : FALSE;
+        lua_settop(L, top);
     }
+
+    /* get packing position from C closure upvalue */
+    luakit_token_t t = (luakit_token_t)lua_tonumber(L, lua_upvalueindex(1));
+
+    if (t == L_TK_PACK1)
+        gtk_paned_pack1(GTK_PANED(w->widget), GTK_WIDGET(child->widget),
+                resize, shrink);
+    else
+        gtk_paned_pack2(GTK_PANED(w->widget), GTK_WIDGET(child->widget),
+                resize, shrink);
     return 0;
+}
+
+static gint
+luaH_paned_get_child(lua_State *L, gint n)
+{
+    widget_t *w = luaH_checkpaned(L, 1);
+    GtkWidget *widget = NULL;
+    if (n == 1)
+        widget = gtk_paned_get_child1(GTK_PANED(w->widget));
+    else
+        widget = gtk_paned_get_child2(GTK_PANED(w->widget));
+
+    if (!widget)
+        return 0;
+
+    widget_t *child = g_object_get_data(G_OBJECT(widget), "lua_widget");
+    luaH_object_push(L, child->ref);
+    return 1;
 }
 
 static gint
@@ -48,8 +84,20 @@ luaH_paned_index(lua_State *L, luakit_token_t token)
       LUAKIT_WIDGET_INDEX_COMMON
       LUAKIT_WIDGET_CONTAINER_INDEX_COMMON
 
-      /* push class methods */
-      PF_CASE(PACK,         luaH_paned_pack)
+      /* push paned widget methods */
+      case L_TK_PACK1:
+      case L_TK_PACK2:
+        lua_pushnumber(L, (gint) token);
+        lua_pushcclosure(L, luaH_paned_pack, 1);
+        return 1;
+
+      case L_TK_TOP:
+      case L_TK_LEFT:
+        return luaH_paned_get_child(L, 1);
+
+      case L_TK_BOTTOM:
+      case L_TK_RIGHT:
+        return luaH_paned_get_child(L, 2);
 
       default:
         break;
@@ -57,27 +105,24 @@ luaH_paned_index(lua_State *L, luakit_token_t token)
     return 0;
 }
 
-#define PANED_WIDGET_CONSTRUCTOR(type)                                       \
-    widget_t *                                                               \
-    widget_##type(widget_t *w)                                               \
-    {                                                                        \
-        w->index = luaH_paned_index;                                         \
-        w->newindex = NULL;                                                  \
-        w->destructor = widget_destructor;                                   \
-        w->widget = gtk_##type##_new();                                      \
-        g_object_set_data(G_OBJECT(w->widget), "lua_widget", (gpointer) w);  \
-        gtk_widget_show(w->widget);                                          \
-        g_object_connect(G_OBJECT(w->widget),                                \
-          "signal::add",        G_CALLBACK(add_cb),        w,                \
-          "signal::parent-set", G_CALLBACK(parent_set_cb), w,                \
-          "signal::remove",     G_CALLBACK(remove_cb),     w,                \
-          NULL);                                                             \
-        return w;                                                            \
+#define PANED_WIDGET_CONSTRUCTOR(type)                                      \
+    widget_t *                                                              \
+    widget_##type(widget_t *w)                                              \
+    {                                                                       \
+        w->index = luaH_paned_index;                                        \
+        w->destructor = widget_destructor;                                  \
+        w->widget = gtk_##type##_new();                                     \
+        g_object_set_data(G_OBJECT(w->widget), "lua_widget", (gpointer) w); \
+        gtk_widget_show(w->widget);                                         \
+        g_object_connect(G_OBJECT(w->widget),                               \
+          "signal::add",        G_CALLBACK(add_cb),        w,               \
+          "signal::parent-set", G_CALLBACK(parent_set_cb), w,               \
+          "signal::remove",     G_CALLBACK(remove_cb),     w,               \
+          NULL);                                                            \
+        return w;                                                           \
     }
 
 PANED_WIDGET_CONSTRUCTOR(vpaned)
 PANED_WIDGET_CONSTRUCTOR(hpaned)
-
-#undef PANED_WIDGET_CONSTRUCTOR
 
 // vim: ft=c:et:sw=4:ts=8:sts=4:tw=80
