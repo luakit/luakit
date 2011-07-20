4ae4e6c8be90536d5449688acdf6256f7494c1ea
karottenreibe <k@rottenrei.be>
2011-07-20 16:43:33 +0200

    added modifier handling on the C side and fixed modifiers on the lua side

--- lib/keysym.lua
+++ lib/keysym.lua
@@ -31,22 +31,7 @@ window.methods.send = function (w, keystring)
             symbol = nil
             modifiers = {}
         elseif symbol and char == "-" then
-            if string.match(symbol, "S(hift)?|C(ontrol)?|L(ock)?|M(od)?[12345]") then
-                for short, long in pairs({
-                    S = "Shift",
-                    C = "Control",
-                    L = "Lock",
-                    M1 = "Mod1",
-                    M2 = "Mod2",
-                    M3 = "Mod3",
-                    M4 = "Mod4",
-                    M5 = "Mod5",
-                }) do
-                    if symbol == short then
-                        symbol = long
-                        break
-                    end
-                end
+            if string.match(symbol, "^[SCL]$") or string.match(symbol, "^M[1-5]$") then
                 table.insert(modifiers, symbol)
                 symbol = ""
             else
