848c60b744788a49bf916b1044373adf41a3d3d2
karottenreibe <k@rottenrei.be>
2011-07-20 15:03:31 +0200

    added the ability to send modifiers as well

--- lib/keysym.lua
+++ lib/keysym.lua
@@ -8,36 +8,52 @@
 -- A keystring is a string of keys to press, with special keys denoted in between
 -- angle brackets:
 --
---  <code>:o google<Tab>something<Return></code>
+--  <code>:o google<S-Tab>something<Return></code>
 --
 -- See gdk/gdkkeysyms.h for a complete list of recognized key names.
 window.methods.send = function (w, keystring)
     local symbol = nil
+    local modifiers = {}
     local keys = {}
     for char in string.gmatch(keystring, ".") do
         if char == "<" then
             symbol = ""
         elseif char == ">" and symbol then
             if #symbol == 0 then
-                table.insert(keys, "<")
-                table.insert(keys, ">")
+                w:error("bad keystring: " .. keystring)
+                return
             else
-                table.insert(keys, symbol)
+                table.insert(keys, {
+                    key = symbol,
+                    mods = modifiers,
+                })
             end
             symbol = nil
-        elseif symbol == nil then
-            table.insert(keys, char)
+            modifiers = {}
+        elseif symbol and char == "-" then
+            if string.match(symbol, "S(hift)?|C(ontrol)?|L(ock)?|M(od)?[12345]") then
+                table.insert(modifiers, symbol)
+                symbol = ""
+            else
+                w:error("bad modifier in keystring: " .. symbol)
+                return
+            end
+        elseif not symbol then
+            table.insert(keys, {
+                key = char,
+                mods = {},
+            })
         else
             symbol = symbol .. char
         end
     end
-    if symbol ~= nil then
+    if symbol then
         w:error("bad keystring: " .. keystring)
         return
     end
     for _, key in ipairs(keys) do
-        if not w.win:send_key(key) then
-            w:error("failed to send key: " .. key)
+        if not w.win:send_key(key.key, key.mods) then
+            w:error("failed to send keystroke: " .. key)
             return
         end
     end
