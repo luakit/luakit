1a79fc8eb45b79c0ca7db7d738d3fe2a7f318f48
karottenreibe <k@rottenrei.be>
2011-07-20 10:14:05 +0200

    send_key now returns true/false based on sending success instead of issuing a console warning

--- widgets/window.c
+++ widgets/window.c
@@ -119,8 +119,8 @@ luaH_window_send_key(lua_State *L)
     }
 
     if (!keyval || keyval == GDK_KEY_VoidSymbol) {
-        warn("cannot simulate key `%s'", key);
-        return 0;
+        lua_pushboolean(L, FALSE);
+        return 1;
     }
 
     GdkKeymapKey* keys;
@@ -138,7 +138,8 @@ luaH_window_send_key(lua_State *L)
 
     gdk_event_put(event);
 
-    return 0;
+    lua_pushboolean(L, TRUE);
+    return 1;
 }
 
 static gint
