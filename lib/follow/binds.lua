a2e9808f1d6432a0d3550a286950ec373ce30e32
Mason Larobina <mason.larobina@gmail.com>
2011-08-13 10:29:49 +0800

    Fix indent and modify follow qmark bind to match ;M(%w)

--- lib/follow/binds.lua
+++ lib/follow/binds.lua
@@ -139,16 +139,17 @@ add_binds("normal", {
     end),
 
 	-- Set command `:qmark <cursor> <uri>`
-	buf("^;M$", function (w,b,m)
-		w:start_follow(modes.uri, "quickmark", function (uri)
-			w:enter_cmd(":qmark  " .. uri, { pos = 7 })
-		end)
-	end),
+    buf("^;M%w$", function (w,b,m)
+        local token = string.match(b, "^;M(.)$")
+        w:start_follow(modes.uri, ":qmark " .. token, function (uri)
+            w:enter_cmd(string.format(":qmark %s %s", token, uri))
+        end)
+    end),
 
-	-- Set command `:bookmark <uri> `
-	buf("^;B$", function (w,b,m)
-		w:start_follow(modes.uri, ":bookmark", function (uri)
-			w:enter_cmd(":bookmark " .. uri .. " ")
-		end)
-	end),
+    -- Set command `:bookmark <uri> `
+    buf("^;B$", function (w,b,m)
+        w:start_follow(modes.uri, ":bookmark", function (uri)
+            w:enter_cmd(":bookmark " .. uri .. " ")
+        end)
+    end),
 })
