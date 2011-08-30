7a7688a43c7c3ee6fda7594db33d20433bbe8be7
karottenreibe <k@rottenrei.be>
2011-08-30 10:19:07 +0200

    missing return caused forms with profile to be ignored

--- lib/formfiller.lua
+++ lib/formfiller.lua
@@ -207,7 +207,7 @@ local DSL = {
         if type(data) == "string" then
             local profile = data
             return function (inputs)
-                transform(inputs, profile)
+                return transform(inputs, profile)
             end
         else
             return transform(data)
@@ -468,6 +468,8 @@ function load(w, fast)
     -- filter out all rules that do not match the current URI
     local rules = filter_rules(w, w.formfiller_state.rules)
     for _, rule in ipairs(rules) do
+        print(rule.pattern)
+        print(#rule.forms)
         -- filter out all forms that do not match the current page
         local forms = filter_forms(w, rule.forms)
         -- assemble a list of menu items to display, if any exist
