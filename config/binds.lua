906926a42cffc18a56b51979bfab7043794fe602
Ben Armston <ben.armston@googlemail.com>
2012-05-31 22:23:29 +0100

    Add tabduplicate command
    Create a new tab, with the same URI as the current tab, and focus the new tab.
    If any arguments are given, they are silently ignored. They should generate an
    error message such as "Trailing characters".

--- config/binds.lua
+++ config/binds.lua
@@ -300,6 +300,7 @@ add_cmds({
     cmd("w[inopen]",            function (w, a) window.new{w:search_open(a)} end),
     cmd("tab",                  function (w, a) w:new_tab() w:run_cmd(":" .. a) end),
     cmd("tabdo",                function (w, a) w:each_tab(function (v) w:run_cmd(":" .. a) end) end),
+    cmd("tabdu[plicate]",       function (w)    w:new_tab(w.view.uri or "") end),
     cmd({"javascript",   "js"}, function (w, a) w:eval_js(a, "javascript") end),
 
     cmd("q[uit]",               function (w, a, o) w:close_win(o.bang) end),
