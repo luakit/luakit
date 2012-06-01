de50435533ca06dd2308c5d87db3eadc8faf6fd5
Mason Larobina <mason.larobina@gmail.com>
2012-06-01 20:26:03 +0800

    JS can return Lua types and registered Lua functions can take JS args
    For example:
        webview:eval_js("1+2;") == 3
    
    And:
        webview:register_function("add", function (a, b) return a+b end)
        webview:eval_js("add(4, 5);") == 9
    
    Exceptions are also supported (in both directions):
        -- Registered function raising JavaScript exception
        webview:register_function("error", function () error("Error!") end)
        -- JavaScript exception caught in Lua
        success, err = pcall(webview.eval_js, webview, "error();");
        print("There was an error:", err)

--- widgets/webview/javascript.c
+++ widgets/webview/javascript.c
@@ -1,7 +1,7 @@
 /*
  * widgets/webview/javascript.c - webkit webview javascript functions
  *
- * Copyright © 2010-2011 Mason Larobina <mason.larobina@gmail.com>
+ * Copyright © 2010-2012 Mason Larobina <mason.larobina@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,136 +20,208 @@
 
 #include <JavaScriptCore/JavaScript.h>
 
-static JSValueRef
-webview_registered_function_callback(JSContextRef context, JSObjectRef fun,
-        JSObjectRef UNUSED(thisObject), size_t UNUSED(argumentCount),
-        const JSValueRef* UNUSED(arguments), JSValueRef *exception)
+static gint
+luaJS_pushstring(lua_State *L, JSContextRef context, JSValueRef value, gchar **error)
 {
-    lua_State *L = globalconf.L;
-    gpointer ref = JSObjectGetPrivate(fun);
-    // get function
-    luaH_object_push(L, ref);
-    // call function
-    gint ret = lua_pcall(L, 0, 0, 0);
-    // handle errors
-    if (ret != 0) {
-        const gchar *exn_cstring = luaL_checkstring(L, -1);
-        lua_pop(L, 1);
-        JSStringRef exn_js_string = JSStringCreateWithUTF8CString(exn_cstring);
-        JSValueRef exn_js_value = JSValueMakeString(context, exn_js_string);
-        *exception = JSValueToObject(context, exn_js_value, NULL);
-        JSStringRelease(exn_js_string);
+    JSStringRef s = JSValueToStringCopy(context, value, NULL);
+    if (!s) {
+        if (error)
+            *error = g_strdup("failed to convert object into string");
+        return 0;
     }
+    size_t size = JSStringGetMaximumUTF8CStringSize(s);
+    gchar cstr[size];
+    JSStringGetUTF8CString(s, cstr, size);
+    lua_pushstring(L, cstr);
+    JSStringRelease(s);
+    return 1;
+}
+
+static gint
+luaJS_raise_exception(lua_State *L, JSContextRef context,
+        JSValueRef exception)
+{
+    if (!luaJS_pushstring(L, context, exception, NULL))
+        lua_pushliteral(L, "Unknown JavaScript Exception (unable to convert "
+                "exception object into string for error message)");
+    lua_error(L);
+    return 0; /* never returns */
+}
+
+/* Push JavaScript value onto Lua stack */
+static gint
+luaJS_pushvalue(lua_State *L, JSContextRef context, JSValueRef value, gchar **error)
+{
+    switch (JSValueGetType(context, value)) {
+      case kJSTypeBoolean:
+        lua_pushboolean(L, JSValueToBoolean(context, value));
+        return 1;
+
+      case kJSTypeNumber:
+        lua_pushnumber(L, JSValueToNumber(context, value, NULL));
+        return 1;
+
+      case kJSTypeString:
+        return luaJS_pushstring(L, context, value, error);
+
+      case kJSTypeUndefined:
+      case kJSTypeNull:
+        lua_pushnil(L);
+        return 1;
+
+      default:
+        break;
+    }
+    return 0;
+}
+
+/* Make JavaScript value from Lua value */
+static JSValueRef
+luaJS_tovalue(lua_State *L, JSContextRef context, gint idx, gchar **error)
+{
+    JSStringRef str;
+    JSValueRef ret;
+
+    switch (lua_type(L, idx)) {
+      case LUA_TBOOLEAN:
+        return JSValueMakeBoolean(context, lua_toboolean(L, idx));
+
+      case LUA_TNUMBER:
+        return JSValueMakeNumber(context, lua_tonumber(L, idx));
+
+      case LUA_TSTRING:
+        str = JSStringCreateWithUTF8CString(lua_tostring(L, idx));
+        ret = JSValueMakeString(context, str);
+        JSStringRelease(str);
+        return ret;
+
+      case LUA_TNIL:
+        return JSValueMakeNull(context);
+
+      case LUA_TNONE:
+        return JSValueMakeUndefined(context);
+
+      default:
+        break;
+    }
+
+    if (error)
+        *error = g_strdup_printf("unhandled Lua->JS type conversion (type %s)",
+                lua_typename(L, lua_type(L, idx)));
     return JSValueMakeUndefined(context);
 }
 
-static void
-webview_collect_registered_function(JSObjectRef obj)
+/* create JavaScript exception object from string */
+static JSValueRef
+luaJS_make_exception(JSContextRef context, const gchar *error)
+{
+    JSStringRef estr = JSStringCreateWithUTF8CString(error);
+    JSValueRef exception = JSValueMakeString(context, estr);
+    JSStringRelease(estr);
+    return JSValueToObject(context, exception, NULL);
+}
+
+static JSValueRef
+luaJS_registered_function_callback(JSContextRef context, JSObjectRef fun,
+        JSObjectRef UNUSED(this), size_t argc, const JSValueRef *argv,
+        JSValueRef *exception)
 {
     lua_State *L = globalconf.L;
-    gpointer ref = JSObjectGetPrivate(obj);
-    luaH_object_unref(L, ref);
+    gint top = lua_gettop(L);
+
+    /* push Lua callback function */
+    luaH_object_push(L, JSObjectGetPrivate(fun));
+
+    gchar *error = NULL;
+    /* push function arguments onto Lua stack */
+    for (guint i = 0; i < argc; i++) {
+        if (luaJS_pushvalue(L, context, argv[i], &error))
+            continue;
+
+        /* raise JavaScript exception */
+        gchar *emsg = g_strdup_printf("bad argument #%d to Lua function (%s)",
+                i, error);
+        *exception = luaJS_make_exception(context, emsg);
+        g_free(error);
+        g_free(emsg);
+        lua_settop(L, top);
+        return JSValueMakeUndefined(context);
+    }
+
+    if (lua_pcall(L, argc, 1, 0)) {
+        /* raise JS exception with Lua error message */
+        const gchar *error = luaL_checkstring(L, -1);
+        lua_pop(L, 1);
+        *exception = luaJS_make_exception(context, error);
+        return JSValueMakeUndefined(context);
+    }
+
+    JSValueRef ret = luaJS_tovalue(L, context, -1, &error);
+    lua_pop(L, 1);
+
+    /* invalid return type from registered function */
+    if (error) {
+        *exception = luaJS_make_exception(context, error);
+        return JSValueMakeUndefined(context);
+    }
+
+    return ret;
 }
 
 static void
-webview_register_function(WebKitWebFrame *frame, const gchar *name, gpointer ref)
+luaJS_registered_function_gc(JSObjectRef obj)
 {
-    JSGlobalContextRef context = webkit_web_frame_get_global_context(frame);
+    gpointer userfunc = JSObjectGetPrivate(obj);
+    luaH_object_unref(globalconf.L, userfunc);
+}
+
+static void
+luaJS_register_function(JSContextRef context, const gchar *name, gpointer ref)
+{
     JSStringRef js_name = JSStringCreateWithUTF8CString(name);
-    // prepare callback function
+
     JSClassDefinition def = kJSClassDefinitionEmpty;
-    def.callAsFunction = webview_registered_function_callback;
+    def.callAsFunction = luaJS_registered_function_callback;
     def.className = g_strdup(name);
-    def.finalize = webview_collect_registered_function;
+    def.finalize = luaJS_registered_function_gc;
     JSClassRef class = JSClassCreate(&def);
+
     JSObjectRef fun = JSObjectMake(context, class, ref);
-    // register with global object
     JSObjectRef global = JSContextGetGlobalObject(context);
-    JSObjectSetProperty(context, global, js_name, fun, kJSPropertyAttributeDontDelete | kJSPropertyAttributeReadOnly, NULL);
-    // release strings
+    JSObjectSetProperty(context, global, js_name, fun,
+            kJSPropertyAttributeDontDelete | kJSPropertyAttributeReadOnly, NULL);
+
     JSStringRelease(js_name);
     JSClassRelease(class);
 }
 
-static gchar*
-webview_eval_js(WebKitWebFrame *frame, const gchar *script, const gchar *source)
+static gint
+luaJS_eval_js(lua_State *L, JSContextRef context, const gchar *script, const gchar *source)
 {
-    JSGlobalContextRef context;
-    JSObjectRef globalobject;
-    JSStringRef js_source;
-    JSStringRef js_script;
-    JSValueRef js_result;
-    JSValueRef js_exc = NULL;
-    JSStringRef js_result_string;
-    GString *result = g_string_new(NULL);
-    size_t js_result_size;
-
-    context = webkit_web_frame_get_global_context(frame);
-    globalobject = JSContextGetGlobalObject(context);
+    JSStringRef js_source, js_script;
+    JSValueRef result, exception = NULL;
 
     /* evaluate the script and get return value*/
     js_script = JSStringCreateWithUTF8CString(script);
     js_source = JSStringCreateWithUTF8CString(source);
-    js_result = JSEvaluateScript(context, js_script, globalobject, js_source, 0, &js_exc);
-    if (js_result && !JSValueIsUndefined(context, js_result)) {
-        js_result_string = JSValueToStringCopy(context, js_result, NULL);
-        js_result_size = JSStringGetMaximumUTF8CStringSize(js_result_string);
 
-        if (js_result_size) {
-            gchar js_result_utf8[js_result_size];
-            JSStringGetUTF8CString(js_result_string, js_result_utf8, js_result_size);
-            g_string_assign(result, js_result_utf8);
-        }
-
-        JSStringRelease(js_result_string);
-    }
-    else if (js_exc) {
-        size_t size;
-        JSStringRef prop, val;
-        JSObjectRef exc = JSValueToObject(context, js_exc, NULL);
-
-        g_printf("Exception occured while executing script:\n");
-
-        /* Print file */
-        prop = JSStringCreateWithUTF8CString("sourceURL");
-        val = JSValueToStringCopy(context, JSObjectGetProperty(context, exc, prop, NULL), NULL);
-        size = JSStringGetMaximumUTF8CStringSize(val);
-        if (size) {
-            gchar cstr[size];
-            JSStringGetUTF8CString(val, cstr, size);
-            g_printf("At %s", cstr);
-        }
-        JSStringRelease(prop);
-        JSStringRelease(val);
-
-        /* Print line */
-        prop = JSStringCreateWithUTF8CString("line");
-        val = JSValueToStringCopy(context, JSObjectGetProperty(context, exc, prop, NULL), NULL);
-        size = JSStringGetMaximumUTF8CStringSize(val);
-        if(size) {
-            gchar cstr[size];
-            JSStringGetUTF8CString(val, cstr, size);
-            g_printf(":%s: ", cstr);
-        }
-        JSStringRelease(prop);
-        JSStringRelease(val);
-
-        /* Print message */
-        val = JSValueToStringCopy(context, exc, NULL);
-        size = JSStringGetMaximumUTF8CStringSize(val);
-        if(size) {
-            gchar cstr[size];
-            JSStringGetUTF8CString(val, cstr, size);
-            g_printf("%s\n", cstr);
-        }
-        JSStringRelease(val);
-    }
+    result = JSEvaluateScript(context, js_script, NULL, js_source, 0, &exception);
 
     /* cleanup */
     JSStringRelease(js_script);
     JSStringRelease(js_source);
 
-    return g_string_free(result, FALSE);
+    if (exception)
+        return luaJS_raise_exception(L, context, exception);
+
+    gchar *error = NULL;
+    gint ret = luaJS_pushvalue(L, context, result, &error);
+    if (error) {
+        lua_pushstring(L, error);
+        g_free(error);
+        lua_error(L);
+    }
+    return ret;
 }
 
 static gint
@@ -173,7 +245,8 @@ luaH_webview_register_function(lua_State *L)
         frame = webkit_web_view_get_main_frame(d->view);
 
     /* register function */
-    webview_register_function(frame, name, ref);
+    JSGlobalContextRef context = webkit_web_frame_get_global_context(frame);
+    luaJS_register_function(context, name, ref);
     return 0;
 }
 
@@ -213,14 +286,12 @@ luaH_webview_eval_js(lua_State *L)
         frame = webkit_web_view_get_main_frame(d->view);
 
     /* evaluate javascript script and push return result onto lua stack */
-    gchar *result = webview_eval_js(frame, script,
+    JSGlobalContextRef context = webkit_web_frame_get_global_context(frame);
+
+    gint ret = luaJS_eval_js(L, context, script,
         usr_source ? usr_source : (const gchar*)source);
 
-    if (result)
-        lua_pushstring(L, result);
-
     g_free(source);
-    g_free(result);
 
-    return result ? 1 : 0;
+    return ret;
 }
