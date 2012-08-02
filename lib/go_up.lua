4177236cae9218c5dda14efb97113d0beef99388
Mason Larobina <mason.larobina@gmail.com>
2012-08-02 22:14:04 +0800

    Refactor go_up.lua into native Lua code (and thanks LokiChaos)

--- lib/go_up.lua
+++ lib/go_up.lua
@@ -1,41 +1,82 @@
-----------------------------------------------------------------
--- Go one step upward in the uri path structure.              --
--- © 2009 Aldrik Dunbar  (n30n)                               --
--- © 2010 Mason Larobina (mason-l) <mason.larobina@gmail.com> --
-----------------------------------------------------------------
+---------------------------------------------------------------------
+-- Go one step upward in the URI path structure.                   --
+-- © 2010-2012 Mason Larobina (mason-l) <mason.larobina@gmail.com> --
+-- © 2012 LokiChaos <loki.chaos@gmail.com>                         --
+---------------------------------------------------------------------
 
-local go_up = [=[
-(function() {
-    try { // Go up one level
-        location = location.href.match(/(\w+:\/\/.+?\/)([\w\?\=\+\%\&\-\.]+\/?)$/)[1];
-    }
-    catch(e) {
-        try { // Removing sub-domain
-            var s = document.domain.match(/^(?!www\.)\w+\.(.+?)\.([a-z]{2,4})(?:\.([a-z]{2}))?$/);
-            var l = s.length;
-            location = location.protocol + "//" + s.slice(1, s[l] ? l : l-1).join(".");
-        }
-        catch(e) {}
-    }
-})();
-]=]
+-- TODO check host against public TLD list to prevent returning only
+-- top-level domain.
 
-local go_upmost = [=[
-(function() {
-    location = location.protocol + "//" + document.domain;
-})();
-]=]
+local parse_uri, uri_tostring = soup.parse_uri, soup.uri_tostring
+local match = string.match
+
+local M = {}
+
+local function go_up_step(u)
+    -- Step 1: remove fragment
+    if u.fragment then
+        u.fragment = nil
+        return
+    end
+
+    -- Step 2: remove query params
+    if u.query then
+        u.query = nil
+        return
+    end
+
+    -- Step 3: remove sub-path from uri
+    local path = u.path
+    if path and path ~= "/" then
+        u.path = match(path, "(.*/)[^/]*/$") or match(path, "(.*/)[^/]+$")
+        return
+    end
+
+    -- Step 4: remove sub-domains from host
+    local host = u.host
+    if host then
+        u.user = nil
+        u.password = nil
+        u.host = match(host, "%.(.+)$") or host
+        return
+    end
+end
+
+function M.go_up(uri, n)
+    local u = soup.parse_uri(uri)
+    if not u then error("invalid uri: " .. tostring(uri)) end
+    for i = 1, (n or 1) do
+        go_up_step(u)
+    end
+    return soup.uri_tostring(u)
+end
+
+function M.go_upmost(uri)
+    local u = soup.parse_uri(uri)
+    if not u then error("invalid uri: " .. tostring(uri)) end
+    u.path = "/"
+    u.fragment = nil
+    u.query = nil
+    return soup.uri_tostring(u)
+end
 
 -- Add `gu` & `gU` binds to the normal mode.
 local buf = lousy.bind.buf
 add_binds("normal", {
-    buf("^gu$", function (w)
-        w.view:eval_js(go_up)
-    end),
+    buf("^gu$", "Go `[count=1]` step upward in the URI path structure.",
+        function (w, _, m)
+            local uri = w.view.uri
+            if not uri or uri == "about:blank" then return end
+            w.view.uri = M.go_up(uri, m.count or 1)
+        end),
 
-    buf("^gU$", function (w)
-        w.view:eval_js(go_upmost)
-    end),
+    buf("^gU$", "Go to up-most URI (maintains host).",
+        function (w)
+            local uri = w.view.uri
+            if not uri or uri == "about:blank" then return end
+            w.view.uri = M.go_upmost(uri)
+        end),
 })
 
--- vim: et:sw=4:ts=8:sts=4:tw=80
+-- Return module table
+return setmetatable(M, { __call = function (M, ...) return M.go_up(...) end })
