4a0911abde2a37ad8bc4891d442fb303e42a8948
Mason Larobina <mason.larobina@gmail.com>
2012-08-23 18:06:41 +0800

    Simulate mouse clicks in go_next_prev when element doesn't have a href

--- lib/go_next_prev.lua
+++ lib/go_next_prev.lua
@@ -6,16 +6,25 @@
 
 local go_next = [=[
 (function() {
-    var el = document.querySelector("[rel='next']");
-    if (el) { // Wow a developer that knows what he's doing!
-        location = el.href;
+    function click(e) {
+        if (e.href)
+            document.location = e.href;
+        else {
+            var ev = document.createEvent("MouseEvent");
+            ev.initMouseEvent("click", true, true, window,
+                0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            e.dispatchEvent(ev);
+        }
     }
+
+    var e = document.querySelector("[rel='next']");
+    if (e) // Wow a developer that knows what he's doing!
+        click(e);
     else { // Search from the bottom of the page up for a next link.
-        var els = document.getElementsByTagName("a");
-        var i = els.length;
-        while ((el = els[--i])) {
-            if (el.text.search(/(\bnext\b|^>$|^(>>|»)$|^(>|»)|(>|»)$|\bmore\b)/i) > -1) {
-                location = el.href;
+        var els = document.getElementsByTagName("a"), i = els.length;
+        while ((e = els[--i])) {
+            if (e.text.search(/(\bnext\b|^>$|^(>>|»)$|^(>|»)|(>|»)$|\bmore\b)/i) > -1) {
+                click(e);
                 break;
             }
         }
@@ -25,16 +34,25 @@ local go_next = [=[
 
 local go_prev = [=[
 (function() {
-    var el = document.querySelector("[rel='prev']");
-    if (el) {
-        location = el.href;
+    function click(e) {
+        if (e.href)
+            document.location = e.href;
+        else {
+            var ev = document.createEvent("MouseEvent");
+            ev.initMouseEvent("click", true, true, window,
+                0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            e.dispatchEvent(ev);
+        }
     }
+
+    var e = document.querySelector("[rel='prev']");
+    if (e)
+        click(e);
     else {
-        var els = document.getElementsByTagName("a");
-        var i = els.length;
-        while ((el = els[--i])) {
-            if (el.text.search(/(\b(prev|previous)\b|^<$|^(<<|«)$|^(<|«)|(<|«)$)/i) > -1) {
-                location = el.href;
+        var els = document.getElementsByTagName("a"), i = els.length;
+        while ((e = els[--i])) {
+            if (e.text.search(/(\b(prev|previous)\b|^<$|^(<<|«)$|^(<|«)|(<|«)$)/i) > -1) {
+                click(e);
                 break;
             }
         }
