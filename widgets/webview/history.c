2c872480bef0db7e9f53e6f588cb63333fc54aec
Jenny Wong <jy2wong@csclub.uwaterloo.ca>
2014-11-04 13:41:09 -0500

    WebKitWebBackForwardList -> WebKitBackForwardList for webkit2 API

--- widgets/webview/history.c
+++ widgets/webview/history.c
@@ -22,10 +22,20 @@ static gint
 luaH_webview_push_history(lua_State *L, WebKitWebView *view)
 {
     /* obtain the history list of the tab and get information about it */
+#if WITH_WEBKIT2
+    WebKitBackForwardList *bflist = webkit_web_view_get_back_forward_list(view);
+    WebKitBackForwardListItem *item;
+    // TODO do these new GLists need to be freed?
+    gint backlen = g_list_length(
+            webkit_back_forward_list_get_back_list(bflist));
+    gint forwardlen = g_list_length(
+            webkit_back_forward_list_get_forward_list(bflist));
+#else
     WebKitWebBackForwardList *bflist = webkit_web_back_forward_list_new_with_web_view(view);
     WebKitWebHistoryItem *item;
     gint backlen = webkit_web_back_forward_list_get_back_length(bflist);
     gint forwardlen = webkit_web_back_forward_list_get_forward_length(bflist);
+#endif
 
     /* compose an overall table with the history list and the position thereof */
     lua_createtable(L, 0, 2);
@@ -38,15 +48,27 @@ luaH_webview_push_history(lua_State *L, WebKitWebView *view)
     lua_createtable(L, backlen + forwardlen + 1, 0);
     for(gint i = -backlen; i <= forwardlen; i++) {
         /* each individual history item is composed of a URL and a page title */
+#if WITH_WEBKIT2
+        item = webkit_back_forward_list_get_nth_item(bflist, i);
+#else
         item = webkit_web_back_forward_list_get_nth_item(bflist, i);
+#endif
         lua_createtable(L, 0, 2);
         /* Set hist_item[uri] = uri */
         lua_pushliteral(L, "uri");
+#if WITH_WEBKIT2
+        lua_pushstring(L, item ? webkit_back_forward_list_item_get_uri(item) : "about:blank");
+#else
         lua_pushstring(L, item ? webkit_web_history_item_get_uri(item) : "about:blank");
+#endif
         lua_rawset(L, -3);
         /* Set hist_item[title] = title */
         lua_pushliteral(L, "title");
+#if WITH_WEBKIT2
+        lua_pushstring(L, item ? webkit_back_forward_list_item_get_title(item) : "");
+#else
         lua_pushstring(L, item ? webkit_web_history_item_get_title(item) : "");
+#endif
         lua_rawset(L, -3);
         lua_rawseti(L, -2, backlen + i + 1);
     }
@@ -58,12 +80,20 @@ luaH_webview_push_history(lua_State *L, WebKitWebView *view)
     return 1;
 }
 
+// TODO this is used to inherit history (say after opening a link in a new tab).
+// the current webkit2 API does not allow a WebKitView's WebKitBackForwardList
+// to be modified in such a way. Look into an alternative. Perhaps completely
+// ignore webkit's backforward list and maintain own copy of history?
 static void
 webview_set_history(lua_State *L, WebKitWebView *view, gint idx)
 {
     gint pos, bflen;
+#if WITH_WEBKIT2
+    WebKitBackForwardList *bflist;
+#else
     WebKitWebBackForwardList *bflist;
     WebKitWebHistoryItem *item = NULL;
+#endif
     gchar *uri = NULL;
 
     if(!lua_istable(L, idx))
@@ -75,8 +105,14 @@ webview_set_history(lua_State *L, WebKitWebView *view, gint idx)
     bflen = lua_objlen(L, -1);
 
     /* create new back-forward history list */
+#if WITH_WEBKIT2
+    bflist = webkit_web_view_get_back_forward_list(view);
+    // TODO no clearing in webkit2 API
+    //webkit_web_back_forward_list_clear(bflist);
+#else
     bflist = webkit_web_back_forward_list_new_with_web_view(view);
     webkit_web_back_forward_list_clear(bflist);
+#endif
 
     /* get position of current history item */
     lua_pushliteral(L, "index");
@@ -94,8 +130,11 @@ webview_set_history(lua_State *L, WebKitWebView *view, gint idx)
         lua_pushliteral(L, "uri");
         lua_rawget(L, -3);
         if (pos || i < bflen) {
+#if !WITH_WEBKIT2
+            // TODO no adding items either in webkit2 API
             item = webkit_web_history_item_new_with_data(lua_tostring(L, -1), NONULL(lua_tostring(L, -2)));
             webkit_web_back_forward_list_add_item(bflist, item);
+#endif
         } else
             uri = g_strdup(lua_tostring(L, -1));
         lua_pop(L, 3);
@@ -106,10 +145,12 @@ webview_set_history(lua_State *L, WebKitWebView *view, gint idx)
         webkit_web_view_load_uri(view, uri);
         g_free(uri);
 
+#if !WITH_WEBKIT2
     /* load item in history */
     } else if (bflen && webkit_web_view_can_go_back_or_forward(view, pos)) {
         webkit_web_view_go_back_or_forward(view, pos);
 
+#endif
     /* load "about:blank" on empty history list */
     } else
         webkit_web_view_load_uri(view, "about:blank");
@@ -138,7 +179,14 @@ luaH_webview_go_back(lua_State *L)
 {
     webview_data_t *d = luaH_checkwvdata(L, 1);
     gint steps = (gint) luaL_checknumber(L, 2);
+#if WITH_WEBKIT2
+    webkit_web_view_go_to_back_forward_list_item(d->view,
+            webkit_back_forward_list_get_nth_item(
+                    webkit_web_view_get_back_forward_list(d->view),
+                            steps * -1));
+#else
     webkit_web_view_go_back_or_forward(d->view, steps * -1);
+#endif
     return 0;
 }
 
@@ -147,6 +195,13 @@ luaH_webview_go_forward(lua_State *L)
 {
     webview_data_t *d = luaH_checkwvdata(L, 1);
     gint steps = (gint) luaL_checknumber(L, 2);
+#if WITH_WEBKIT2
+    webkit_web_view_go_to_back_forward_list_item(d->view,
+            webkit_back_forward_list_get_nth_item(
+                    webkit_web_view_get_back_forward_list(d->view),
+                            steps));
+#else
     webkit_web_view_go_back_or_forward(d->view, steps);
+#endif
     return 0;
 }
