44d306d529aaa1c0abf9d6df822121df5814c745
Aidan Holm <aidanholm@gmail.com>
2016-04-04 23:03:21 +0800

    Add modeline to extension/main.c

--- extension/main.c
+++ extension/main.c
@@ -3,5 +3,7 @@
 G_MODULE_EXPORT void
 webkit_web_extension_initialize(WebKitWebExtension *extension)
 {
-	puts("luakit.so loaded");
+    puts("luakit.so loaded");
 }
+
+// vim: ft=c:et:sw=4:ts=8:sts=4:tw=80
