96d90534438ac09f17613dbb8edb0ee1652aa728
Aidan Holm <aidanholm@gmail.com>
2016-05-20 09:36:13 +0800

    Move Lua JS stuff to common/

---
+++ common/luajs.h
@@ -0,0 +1,17 @@
+#ifndef LUAKIT_COMMON_LUAJS_H
+#define LUAKIT_COMMON_LUAJS_H
+
+#include <JavaScriptCore/JavaScript.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <lua.h>
+
+gchar* tostring(JSContextRef context, JSValueRef value, gchar **error);
+gint luaJS_pushstring(lua_State *L, JSContextRef context, JSValueRef value, gchar **error);
+gint luaJS_pushobject(lua_State *L, JSContextRef context, JSObjectRef obj, gchar **error);
+gint luaJS_pushvalue(lua_State *L, JSContextRef context, JSValueRef value, gchar **error);
+JSValueRef luaJS_fromtable(lua_State *L, JSContextRef context, gint idx, gchar **error);
+JSValueRef luaJS_tovalue(lua_State *L, JSContextRef context, gint idx, gchar **error);
+JSValueRef luaJS_make_exception(JSContextRef context, const gchar *error);
+
+#endif /* end of include guard: LUAKIT_COMMON_LUAJS_H */
