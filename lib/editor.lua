4b35842f0eb11c615e3ebef9e988766969c3a9fd
Aidan Holm <aidanholm@gmail.com>
2016-06-08 10:34:37 +0800

    editor.lib: fall back to termite and nvim

--- lib/editor.lua
+++ lib/editor.lua
@@ -1,14 +1,15 @@
 local string = string
 local globals = globals
 local capi = { luakit = luakit }
+local os = os
 
 module "editor"
 
 -- Can't yet handle files with special characters in their name
 edit = function (file, line)
 	local subs = {
-		term = globals.term or "xterm",
-		editor = globals.editor or "vim",
+		term = globals.term or os.getenv("TERMINAL") or "xterm",
+		editor = globals.editor or os.getenv("EDITOR") or "vim",
 		file = file,
 		line = line or 0,
 	}
