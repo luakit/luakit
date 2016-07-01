4b4bde0228eabab6eb52e5763996a458e84ec83c
Aidan Holm <aidanholm@gmail.com>
2016-07-02 03:11:30 +0800

    Revert "Make go_next_prev work without JS enabled"
    This reverts commit 9f384608f38a8345b1ff5532ffe8ce93ad16fb8e.

--- lib/go_next_prev.lua
+++ lib/go_next_prev.lua
@@ -4,13 +4,67 @@
 -- © 2010 Mason Larobina (mason-l) <mason.larobina@gmail.com> --
 ----------------------------------------------------------------
 
-local go_next_prev_wm = web_module("go_next_prev_webmodule")
+local go_next = [=[
+(function() {
+    function click(e) {
+        if (e.href)
+            document.location = e.href;
+        else {
+            var ev = document.createEvent("MouseEvent");
+            ev.initMouseEvent("click", true, true, window,
+                0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            e.dispatchEvent(ev);
+        }
+    }
+
+    var e = document.querySelector("[rel='next']");
+    if (e) // Wow a developer that knows what he's doing!
+        click(e);
+    else { // Search from the bottom of the page up for a next link.
+        var els = document.getElementsByTagName("a"), i = els.length;
+        while ((e = els[--i])) {
+            if (e.text.search(/(\bnext\b|^>$|^(>>|»)$|^(>|»)|(>|»)$|\bmore\b)/i) > -1) {
+                click(e);
+                break;
+            }
+        }
+    }
+})();
+]=]
+
+local go_prev = [=[
+(function() {
+    function click(e) {
+        if (e.href)
+            document.location = e.href;
+        else {
+            var ev = document.createEvent("MouseEvent");
+            ev.initMouseEvent("click", true, true, window,
+                0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            e.dispatchEvent(ev);
+        }
+    }
+
+    var e = document.querySelector("[rel='prev']");
+    if (e)
+        click(e);
+    else {
+        var els = document.getElementsByTagName("a"), i = els.length;
+        while ((e = els[--i])) {
+            if (e.text.search(/(\b(prev|previous)\b|^<$|^(<<|«)$|^(<|«)|(<|«)$)/i) > -1) {
+                click(e);
+                break;
+            }
+        }
+    }
+})();
+]=]
 
 -- Add `[[` & `]]` bindings to the normal mode.
 local buf = lousy.bind.buf
 add_binds("normal", {
-    buf("^%]%]$", function (w) go_next_prev_wm:emit_signal("go", w.view.id, "next") end),
-    buf("^%[%[$", function (w) go_next_prev_wm:emit_signal("go", w.view.id, "prev") end),
+    buf("^%]%]$", function (w) w.view:eval_js(go_next, { no_return = true }) end),
+    buf("^%[%[$", function (w) w.view:eval_js(go_prev, { no_return = true }) end),
 })
 
 -- vim: et:sw=4:ts=8:sts=4:tw=80
