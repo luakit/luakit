09e863ee1efa94a75f1d74ab41327454746c8a32
Aidan Holm <aidanholm@gmail.com>
2016-07-03 17:40:36 +0800

    Remove Pre-GTK3 eventbox widget support

--- widgets/eventbox.c
+++ widgets/eventbox.c
@@ -43,11 +43,7 @@ luaH_eventbox_newindex(lua_State *L, widget_t *w, luakit_token_t token)
 {
     size_t len;
     const gchar *tmp;
-#if GTK_CHECK_VERSION(3,0,0)
     GdkRGBA c;
-#else
-    GdkColor c;
-#endif
 
     switch(token) {
       LUAKIT_WIDGET_NEWINDEX_COMMON(w)
@@ -55,18 +51,12 @@ luaH_eventbox_newindex(lua_State *L, widget_t *w, luakit_token_t token)
 
       case L_TK_BG:
         tmp = luaL_checklstring(L, 3, &len);
-#if GTK_CHECK_VERSION(3,0,0)
         if (!gdk_rgba_parse(&c, tmp))
-#else
-        if (!gdk_color_parse(tmp, &c))
-#endif
             luaL_argerror(L, 3, "unable to parse colour");
 #if GTK_CHECK_VERSION(3,16,0)
         widget_set_css_properties(w, "background-color", tmp, NULL);
-#elif GTK_CHECK_VERSION(3,0,0)
-        gtk_widget_override_background_color(GTK_WIDGET(w->widget), GTK_STATE_FLAG_NORMAL, &c);
 #else
-        gtk_widget_modify_bg(GTK_WIDGET(w->widget), GTK_STATE_NORMAL, &c);
+        gtk_widget_override_background_color(GTK_WIDGET(w->widget), GTK_STATE_FLAG_NORMAL, &c);
 #endif
         g_object_set_data_full(G_OBJECT(w->widget), "bg", g_strdup(tmp), g_free);
         break;
