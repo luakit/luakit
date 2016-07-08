ffd43c4f8ccb04087dd809b789936f9b413f8279
Aidan Holm <aidanholm@gmail.com>
2016-07-08 23:06:24 +0800

    Follow mode: click child element at centre of target

--- lib/follow_webmodule.lua
+++ lib/follow_webmodule.lua
@@ -92,6 +92,13 @@ evaluators = {
                 element:focus()
                 return "form-active"
             end
+        elseif element.child_count > 0 then
+            -- Find the element directly in the centre of the link,
+            -- and click that
+            local r = element.rect
+            local doc = element.owner_document
+            local mid_elem = doc:element_from_point(r.left + r.width/2, r.top + r.height/2)
+            mid_elem:click()
         else
             element:click()
         end
@@ -341,7 +348,12 @@ end
 
 local function follow_hint(state, hint)
     local evaluator = evaluators[state.evaluator]
+
+    local overlay_style = hint.overlay_elem.attr.style
+    hint.overlay_elem.attr.style = "display: none;"
     local ret = evaluator(hint.elem)
+    hint.overlay_elem.attr.style = overlay_style
+
     ui:emit_signal("follow_func", state.wid, ret)
 end
 
