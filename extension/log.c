09eb7d9cc1a5c3f2c23376024fbc798ffc9ef693
Aidan Holm <aidanholm@gmail.com>
2016-08-26 18:37:44 +0800

    Correct extension/log.c header comment

--- extension/log.c
+++ extension/log.c
@@ -1,5 +1,5 @@
 /*
- * common/log.c - logging functions
+ * extension/log.c - logging interface for web extension
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
