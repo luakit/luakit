8e02e13ec0b4927de99a03f81c42981002c91b57
Aidan Holm <aidanholm@gmail.com>
2017-03-03 15:24:40 +0800

    Remove useless code from buffer widget

--- lib/widget/buf.lua
+++ lib/widget/buf.lua
@@ -21,13 +21,6 @@ window.add_signal("init", function (w)
     -- Set style
     r.buf.fg = theme.buf_sbar_fg
     r.buf.font = theme.buf_sbar_font
-
-    -- Update widget when page switched... WHY
-    w.tabs:add_signal("switch-page", function ()
-        luakit.idle_add(function ()
-            update(w)
-        end)
-    end)
 end)
 
 window.methods.update_buf = update
