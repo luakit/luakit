3dc0ba9ffc695666ef1379afe9fc26ec27f12aff
Aidan Holm <aidanholm@gmail.com>
2017-03-04 15:38:10 +0800

    Add test for header comment format

--- tests/test_source_format.lua
+++ tests/test_source_format.lua
@@ -85,3 +85,67 @@ function test_include_guard ()
         fail("Some files do not have include guards:\n" .. table.concat(err, "\n"))
     end
 end
+
+function test_header_comment ()
+    local file_desc_pat = "^%/%*\n %* (%S+) %- [^\n]*\n %*\n"
+    local copyright_pat = " %* Copyright © [^\n]*\n"
+    local gpl_text = [[
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+]]
+    local missing = {}
+
+    for file in files(".", "%.[ch]$") do
+        -- Get first paragraph of file
+        local f = assert(io.open(file, "r"))
+        local lines = {}
+        for line in f:lines() do
+            lines[#lines + 1] = line
+            if line == "" then break end
+        end
+        local contents = table.concat(lines, "\n") .. "\n"
+        f:close()
+
+        local file_desc_name = contents:match(file_desc_pat)
+        local bad_file_desc = file_desc_name and file_desc_name ~= file:sub(3)
+        local no_copyright = not contents:find(copyright_pat)
+        local no_gpl_text = not contents:find(gpl_text, 1, true)
+
+        if bad_file_desc or no_copyright or no_gpl then
+            local errors = {}
+            if bad_file_desc then errors[#errors+1] = "file description" end
+            if no_copyright then errors[#errors+1] = "copyright line" end
+            if no_gpl_text then errors[#errors+1] = "GPL license text" end
+            table.insert(missing, {
+                file = file,
+                err = table.concat(errors, ", ")
+            })
+        end
+    end
+
+    if #missing > 0 then
+        local align = 0
+        for _, entry in ipairs(missing) do
+            align = math.max(align, entry.file:len())
+        end
+
+        local err = {}
+        for _, entry in ipairs(missing) do
+            err[#err+1] = string.format("  %-" .. tostring(align-1) .. "s bad/missing %s", entry.file:sub(3), entry.err)
+        end
+        fail("Some files have header comment errors:\n" .. table.concat(err, "\n"))
+    end
+end
