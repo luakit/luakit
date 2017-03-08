da462f4bb24fc43348bf22438e525e5e883a2dc5
Aidan Holm <aidanholm@gmail.com>
2017-03-08 20:38:46 +0800

    tests: Add test for trailing whitespace

--- tests/style/test_whitespace.lua
+++ tests/style/test_whitespace.lua
@@ -36,6 +36,34 @@ function T.test_no_tabs_in_indentation ()
     end
 end
 
+function T.test_no_trailing_whitespace ()
+    local exclude_files = { "lib/markdown%.lua" }
+
+    local errors = {}
+    local file_list = {}
+    file_list = join_table(file_list, util.find_files(".", "%.lua$", exclude_files))
+    file_list = join_table(file_list, util.find_files(".", "%.[ch]$", exclude_files))
+
+    for _, file in ipairs(file_list) do
+        local lines = {}
+        local f = assert(io.open(file, "r"))
+        for line in f:lines() do
+           lines[#lines+1] = line
+        end
+        f:close()
+
+        for i, line in ipairs(lines) do
+            if line:match("%s$") then
+                add_file_error(errors, file .. ":" .. i, "Trailing whitespace")
+            end
+        end
+    end
+
+    if #errors > 0 then
+        test.fail("Some files have trailing whitespace:\n" .. util.format_file_errors(errors))
+    end
+end
+
 return T
 
 -- vim: et:sw=4:ts=8:sts=4:tw=80
