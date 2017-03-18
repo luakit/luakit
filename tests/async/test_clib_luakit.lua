9f8ffc3ccb349fb3ee165e6bfdc495271fd2fc81
Aidan Holm <aidanholm@gmail.com>
2017-03-18 17:14:52 +0800

    tests: Migrate tests/lunit/test_clib_luakit.lua

---
+++ tests/async/test_clib_luakit.lua
@@ -0,0 +1,75 @@
+--- Test luakit clib functionality.
+--
+-- @copyright Mason Larobina
+
+local assert = require "luassert"
+
+local T = {}
+
+T.test_luakit = function ()
+    assert.is_table(luakit)
+    -- Check metatable
+    local mt = getmetatable(luakit)
+    assert.is_function(mt.__index, "luakit mt missing __index")
+end
+
+T.test_luakit_index = function ()
+    local funcprops = { "exec", "quit", "save_file", "spawn", "spawn_sync",
+        "time", "uri_decode", "uri_encode", "idle_add", "idle_remove" }
+    for _, p in ipairs(funcprops) do
+        assert.is_function(luakit[p], "Missing/invalid function: luakit."..p)
+    end
+
+    local strprops = { "cache_dir", "config_dir", "data_dir", "execpath",
+        "confpath", "install_path", "version" }
+    for _, p in ipairs(strprops) do
+        assert.is_string(luakit[p], "Missing/invalid property: luakit."..p)
+    end
+
+    local boolprops = { "dev_paths", "verbose", "nounique" }
+    for _, p in ipairs(boolprops) do
+        assert.is_boolean(luakit[p], "Missing/invalid property: luakit."..p)
+    end
+
+    assert.is_number(luakit.time(), "Invalid: luakit.time()")
+end
+
+T.test_webkit_version = function ()
+    assert.is_match("^%d+%.%d+%.%d$", luakit.webkit_version,
+        "Invalid format: luakit.webkit_version")
+    assert.is_match("^%d+%.%d+$", luakit.webkit_user_agent_version,
+        "Invalid format: luakit.webkit_user_agent_version")
+end
+
+T.test_windows_table = function ()
+    assert.is_table(luakit.windows, "Missing/invalid luakit.windows table.")
+    assert.is_equal(#luakit.windows, 0, "Invalid number of windows")
+    local win = widget{type="window"}
+    assert.is_equal(#luakit.windows, 1,
+        "luakit.windows not tracking opened windows.")
+    win:destroy()
+    assert.is_equal(#luakit.windows, 0,
+        "luakit.windows not tracking closed windows.")
+end
+
+T.test_invalid_prop = function ()
+    assert.is_nil(luakit.invalid_property)
+end
+
+T.test_idle_add_del = function ()
+    local f = function () end
+    assert.is_false(luakit.idle_remove(f),
+        "Function can't be removed before it's been added.")
+    for _ = 1,5 do
+        luakit.idle_add(f)
+    end
+    for _ = 1,5 do
+        assert.is_true(luakit.idle_remove(f), "Error removing callback.")
+    end
+    assert.is_false(luakit.idle_remove(f),
+        "idle_remove removed incorrect number of callbacks.")
+end
+
+return T
+
+-- vim: et:sw=4:ts=8:sts=4:tw=80
