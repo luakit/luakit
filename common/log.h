bad11d693a998a0a151ccc9ffba9ef9c973a8d72
Aidan Holm <aidanholm@gmail.com>
2017-03-28 20:05:34 +0800

    Add missing ANSI color #defines

--- common/log.h
+++ common/log.h
@@ -39,12 +39,14 @@ typedef enum { LOG_LEVELS } log_level_t;
 /* ANSI term color codes */
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+#define ANSI_COLOR_BLACK   "\x1b[30m"
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_BLUE    "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_GRAY    "\x1b[37m"
 
 #define ANSI_COLOR_BG_RED  "\x1b[41m"
 
