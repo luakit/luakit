b67a649c490d6bf8c553e6a77393f7fb6c331ac8
Aidan Holm <aidanholm@gmail.com>
2017-04-11 11:44:24 +0800

    docs: Add documentation for extension class

---
+++ doc/luadoc/extension.lua
@@ -0,0 +1,13 @@
+-- Main module for web extensions
+-- @module extension
+-- @author Aidan Holm
+-- @copyright 2016 Aidan Holm
+--
+-- <div class="alert"><i>This class is only available from web process Lua states.</i></div>
+
+--- @signal page-created
+--
+-- Emitted after the creation of a [`page`](../classes/page.html) object; i.e. after
+-- a new webview widget has been created in the UI process.
+
+-- vim: et:sw=4:ts=8:sts=4:tw=80
