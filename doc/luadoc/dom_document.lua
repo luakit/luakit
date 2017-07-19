89051e0cb1812721cab80f8582f63ded37c5262d
Aidan Holm <aidanholm@gmail.com>
2017-07-20 01:07:07 +0800

    Add missing documentation

---
+++ doc/luadoc/dom_document.lua
@@ -0,0 +1,50 @@
+--- Web process Lua interface to the DOM
+-- @class dom_document
+-- @author Aidan Holm
+-- @copyright 2017 Aidan Holm
+-- @prefix document
+--
+-- DOCMACRO(available:web)
+--
+-- ### Retrieving the DOM document for a `page`:
+--
+-- To retrieve the DOM document loaded in a page, use the `document` property.
+
+--- @method create_element
+-- Create a new element for the DOM document.
+-- @tparam string tag_name The tag name of the element to create.
+-- @tparam[opt] table attributes A table of attributes to set on the new element. All keys and values must be strings.
+-- @tparam[opt] string inner_text The inner text to set on the element.
+
+--- @method element_from_point
+-- Find the DOM element at the given point.
+-- @tparam integer x The X coordinate of the point.
+-- @tparam integer y The Y coordinate of the point.
+-- @treturn dom_element The element at the given point.
+
+--- @property body
+-- The body of the DOM document.
+-- @type dom_element
+-- @readonly
+
+--- @property window.scroll_x
+-- The horizontal scroll position of the document view.
+-- @type integer
+-- @readonly
+
+--- @property window.scroll_y
+-- The vertical scroll position of the document view.
+-- @type integer
+-- @readonly
+
+--- @property window.inner_width
+-- The inner width of the DOM document.
+-- @type integer
+-- @readonly
+
+--- @property window.inner_height
+-- The inner height of the DOM document.
+-- @type integer
+-- @readonly
+
+-- vim: et:sw=4:ts=8:sts=4:tw=80
---
+++ doc/luadoc/download.lua
@@ -0,0 +1,93 @@
+--- Luakit download interface
+--
+-- This class provides an interface to managing ongoing downloads.
+--
+-- @class download
+-- @author Fabian Streitel
+-- @author Mason Larobina 
+-- @copyright 2011 Fabian Streitel
+-- @copyright 2011 Mason Larobina 
+
+--- @property allow_overwrite
+-- Whether this download should be permitted to overwrite any file already
+-- existing at the destination.
+-- @type boolean
+-- @readwrite
+-- @default `false`
+
+--- @property destination
+-- The path at which the downloaded file should be saved.
+-- @type string
+-- @readwrite
+-- @default `nil`
+
+--- @property progress
+-- The download progress, ranging from 0.0 (no data yet received) to 1.0 (all
+-- data received). This is only an estimate; see the `current_size` property for
+-- a more exact value.
+-- @type number
+-- @readonly
+
+--- @property status
+-- The download status. Will be one of `"created"`, `"started"`, `"cancelled"`,
+-- `"finished"`, or `"failed"`.
+-- @type string
+-- @readonly
+
+--- @property error
+-- The error message for the download, or nil.
+-- @type string|nil
+-- @readonly
+
+--- @property total_size
+-- The total size of the file to be downloaded, including all data not yet
+-- downloaded.
+-- @type number
+-- @readonly
+
+--- @property current_size
+-- The current size of all data that has been downloaded and written to the
+-- destination.
+-- @type number
+-- @readonly
+
+--- @property elapsed_time
+-- The length of time that the download has been running, in seconds. This
+-- includes any fractional part.
+-- @type number
+-- @readonly
+
+--- @property mime_type
+-- The MIME type of the download, if known.
+-- @type string|nil
+-- @readonly
+
+--- @property suggested_filename
+-- The suggested filename to use, for use in dialog boxes.
+-- @type string
+-- @readonly
+
+--- @signal property::allow-overwrite
+-- Emitted when the `allow_overwrite` property has changed.
+
+--- @signal property::destination
+-- Emitted when the `destination` property has changed.
+
+--- @signal decide-destination
+-- Emitted when a destination for the download must be decided. Handlers should
+-- set the download's `destination` property.
+-- @tparam string suggested_filename The suggested filename for the download.
+-- @treturn boolean `true` if the destination was decided.
+
+--- @signal created-destination
+-- Emitted when the destination has been created.
+-- @tparam string destination The final destination for the download.
+
+--- @signal error
+-- Emitted when the download fails.
+-- @tparam string message The error message.
+
+--- @signal finished
+-- Emitted when the download has finished.
+
+-- vim: et:sw=4:ts=8:sts=4:tw=80
