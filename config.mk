669007357ad56103f97cd2edb6d35b946cfb1ec7
Aidan Holm <aidanholm@gmail.com>
2017-07-27 20:23:31 +0800

    Make DEVELOPMENT_PATHS=0 by default

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ CPPFLAGS   += -DVERSION=\"$(VERSION)\"
 
 # === Default build options ==================================================
 
-DEVELOPMENT_PATHS ?= 1
+DEVELOPMENT_PATHS ?= 0
 USE_LUAJIT        ?= 1
 
 # === Paths ==================================================================
