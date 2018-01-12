f50f3a0d59e92fedf41c3547754b1be84c09dc8c
Aidan Holm <aidanholm@gmail.com>
2018-01-12 22:49:13 +0800

    follow: fix intermittent failure on target=_blank links (#651)
    Failure was caused by checking after the element at the centre had been
    determined.

--- lib/follow_wm.lua
+++ lib/follow_wm.lua
@@ -20,18 +20,17 @@ local evaluators = {
                 return "form-active"
             end
         end
-        -- Find the element directly in the centre of the link
-        if element.child_count > 0 then
-            local r = element.rect
-            local doc = element.owner_document
-            element = doc:element_from_point(r.left + r.width/2, r.top + r.height/2) or element
-            tag = element.tag_name
-        end
         -- Handle <a target=_blank> indirectly; WebKit prevents opening a new
         -- window if not initiated by the user directly
         if tag == "A" and element.attr.target == "_blank" then
             ui:emit_signal("click_a_target_blank", page.id, element.href)
             return
+        end
+        -- Find the element directly in the centre of the link
+        if element.child_count > 0 then
+            local r = element.rect
+            local doc = element.owner_document
+            element = doc:element_from_point(r.left + r.width/2, r.top + r.height/2) or element
         end
         element:click()
     end,
