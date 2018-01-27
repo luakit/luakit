b6115b88b0bf6063052b72ef98967fc115c303d0
Aidan Holm <aidanholm@gmail.com>
2018-01-27 08:10:28 +0800

    Make luaH_dump_stack() non-inline

--- common/lualib.h
+++ common/lualib.h
@@ -80,43 +80,7 @@ luaH_dump_table_keys(lua_State *L, gint idx)
     g_fprintf(stderr, "and %d more\n", rem);
 }
 
-/** Dump the Lua stack. Useful for debugging.
- * \param L The Lua VM state.
- */
-static inline void
-luaH_dump_stack(lua_State *L) {
-    g_fprintf(stderr, "-------- Lua stack dump ---------\n");
-    for(int i = lua_gettop(L); i; i--) {
-        int t = lua_type(L, i);
-        switch (t) {
-          case LUA_TSTRING:
-            g_fprintf(stderr, "%d: string: `%s'\n", i, lua_tostring(L, i));
-            break;
-          case LUA_TBOOLEAN:
-            g_fprintf(stderr, "%d: bool:   %s\n", i, lua_toboolean(L, i) ? "true" : "false");
-            break;
-          case LUA_TNUMBER:
-            g_fprintf(stderr, "%d: number: %g\n", i, lua_tonumber(L, i));
-            break;
-          case LUA_TNIL:
-            g_fprintf(stderr, "%d: nil\n", i);
-            break;
-          case LUA_TUSERDATA:
-            g_fprintf(stderr, "%d: <%s>\t\t%p\n", i, luaH_typename(L, i), lua_topointer(L, i));
-            break;
-          case LUA_TTABLE:
-            g_fprintf(stderr, "%d: table\t#%zu\t%p\n", i, lua_objlen(L, i), lua_topointer(L, i));
-            luaH_dump_table_keys(L, i);
-            break;
-          default:
-            g_fprintf(stderr, "%d: %s\t#%d\t%p\n", i, lua_typename(L, t),
-                    (gint) lua_objlen(L, i),
-                    lua_topointer(L, i));
-            break;
-        }
-    }
-    g_fprintf(stderr, "------- Lua stack dump end ------\n");
-}
+void luaH_dump_stack(lua_State *L);
 
 /** Convert s stack index to positive.
  * \param L The Lua VM state.
