d3d8e0a7524aaae494f36b7fad8916932370e930
Yorick Hardy <yorickhardy@gmail.com>
2018-11-16 20:22:37 +0200

    only signal "mime-type-decision" for a succesful response

--- widgets/webview.c
+++ widgets/webview.c
@@ -487,6 +487,9 @@ decide_policy_cb(WebKitWebView* UNUSED(v), WebKitPolicyDecision *p,
         const gchar *uri = webkit_uri_response_get_uri(r);
         const gchar *mime = webkit_uri_response_get_mime_type(r);
 
+        if(!SOUP_STATUS_IS_SUCCESSFUL(webkit_uri_response_get_status_code(r)))
+            return FALSE;
+
         luaH_object_push(L, w->ref);
         lua_pushstring(L, uri);
         lua_pushstring(L, mime);
