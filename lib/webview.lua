luakit/luakit

commit e3338495323b8d696c2bc53e4e6d0011bb4d0e99

Honor window.new_tab_page for cases where tab is opened without uri set
This sets the tab uri to window.new_tab_page when there is no uri passed
to new_tab (e.g. when calling :open or :tabopen without any parameters).
It also uses new_tab_page when restoring sessions with empty tabs.

Fixes #752

lib/window.lua
@@ -463,6 +463,9 @@ _M.methods = {
         if not view then
             -- Make new webview widget
             view = webview.new({ private = opts.private })
+            if not arg then
+                view.uri = settings.get_setting("window.new_tab_page")
+            end
             w:attach_tab(view, switch, order)
         end
 

lib/session.lua
@@ -128,10 +128,10 @@ local restore_file = function (file, delete)
         for _, item in ipairs(win.open) do
             local v
             if not w then
-                w = window.new({"about:blank"})
+                w = window.new({settings.get_setting("window.new_tab_page")})
                 v = w.view
             else
-                v = w:new_tab("about:blank", { switch = item.current })
+                v = w:new_tab(settings.get_setting("window.new_tab_page"), { switch = item.current })
             end
             -- Block the tab load, then set its location
             webview.modify_load_block(v, "session-restore", true)

commit dd2b7ae07e4fee503c2ed7845d8f392c35858457

Fix tabs opened in background being blank
This issue was introduced by e333849; WebKit requires that tabs created
for related views (i.e. in response to the GTK "create" signal) do
not have a URL set, but that commit caused the default to be the
luakit://newtab/ page.

This commit fixes that issue by introducing an optional parameter to
prevent this behaviour. Slightly ugly, but does the trick.

Fixes #834.
Fixes #854.

lib/webview.lua
@@ -130,7 +130,8 @@ local init_funcs = {
     create_webview = function (view)
         -- Return a newly created webview in a new tab
         view:add_signal("create-web-view", function (v)
-            return _M.window(v):new_tab(nil, { private = v.private })
+            local opts = { private = v.private, no_initial_url = true }
+            return _M.window(v):new_tab(nil, opts)
         end)
     end,
 

lib/window.lua
@@ -463,7 +463,7 @@ _M.methods = {
         if not view then
             -- Make new webview widget
             view = webview.new({ private = opts.private })
-            if not arg then
+            if not arg and not opts.no_initial_url then
                 view.uri = settings.get_setting("window.new_tab_page")
             end
             w:attach_tab(view, switch, order)