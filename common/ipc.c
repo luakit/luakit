406de7ab834c1b7035097b32617681bb00a777e0
c0dev0id <sh+github@codevoid.de>
2020-09-20 11:22:32 +0200

    Handle EOF sent to G_IO_IN instead of G_IO_HUP (NetBSD/OSX)

--- common/ipc.c
+++ common/ipc.c
@@ -21,6 +21,7 @@
 #include "common/lualib.h"
 #include "common/luaserialize.h"
 #include "common/ipc.h"
+#include "log.h"
 
 /* Prototypes for ipc_recv_... functions */
 #define X(name) void ipc_recv_##name(ipc_endpoint_t *ipc, const void *msg, guint length);
@@ -138,6 +139,18 @@ ipc_recv_and_dispatch_or_enqueue(ipc_endpoint_t *ipc)
         case G_IO_STATUS_AGAIN:
             return;
         case G_IO_STATUS_EOF:
+            verbose("g_io_channel_read_chars(): End Of File received");
+            /* OSX and NetBSD are sending EOF on nonblocking channels first.
+             * This EOF can be ignored. They should end up in recv_hup(),
+             * but unfortunaltey they do not.
+             *
+             * In order to avoid an incref/decref loop. The refcount is
+             * forced down here until it reaches 1. Once 1 is reached,
+             * the decref call after this function will bring it to 0 and
+             * clean up the channel.
+             */
+            if(&ipc->refcount > 1)
+                g_atomic_int_dec_and_test(&ipc->refcount);
             return;
         case G_IO_STATUS_ERROR:
             if (!g_str_equal(ipc->name, "UI"))
