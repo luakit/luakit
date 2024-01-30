luakit/luakit

commit e400cda67c814614a42b650284ed9a0c2f330257

Remove G_REGEX_OPTIMIZE
This removes the warning:
GLib-DEBUG (recursed): JIT compilation was requested with G_REGEX_OPTIMIZE, but JIT support is not available. Falling back to interpretive code.

And also fixes https://github.com/luakit/luakit/issues/1071

common/clib/soup.h
@@ -161,7 +161,7 @@ luaH_soup_parse_uri(lua_State *L)
 static void
 soup_lib_setup_common(void)
 {
-    scheme_reg = g_regex_new("^[a-z][a-z0-9\\+\\-\\.]*:", G_REGEX_OPTIMIZE, 0, NULL);
+    scheme_reg = g_regex_new("^[a-z][a-z0-9\\+\\-\\.]*:", NULL, 0, NULL);
 }
 
 #endif

log.c
@@ -222,7 +222,7 @@ va_log(log_level_t lvl, const gchar *fct, const gchar *fmt, va_list ap)
     static GRegex *indent_lines_reg;
     if (!indent_lines_reg) {
         GError *err = NULL;
-        indent_lines_reg = g_regex_new("\n", G_REGEX_OPTIMIZE, 0, &err);
+        indent_lines_reg = g_regex_new("\n", NULL, 0, &err);
         g_assert_no_error(err);
     }
     gchar *wrapped = g_regex_replace_literal(indent_lines_reg, msg, -1, 0, "\n" LOG_IND, 0, NULL);

common/clib/regex.c
@@ -59,7 +59,7 @@ luaH_regenerate_regex(lua_State *L, lregex_t *regex)
 
     GError *error = NULL;
     regex->reg = g_regex_new(regex->pattern,
-            G_REGEX_DOTALL|G_REGEX_OPTIMIZE, 0, &error);
+            G_REGEX_DOTALL, 0, &error);
     if (error) {
         lua_pushstring(L, error->message);
         g_error_free(error);

common/util.c
@@ -63,7 +63,7 @@ strip_ansi_escapes(const gchar *in)
     if (!reg) {
         const gchar *expr = "[\x1b\x9b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]";
         GError *err = NULL;
-        reg = g_regex_new(expr, G_REGEX_DOTALL | G_REGEX_EXTENDED | G_REGEX_RAW | G_REGEX_OPTIMIZE, 0, &err);
+        reg = g_regex_new(expr, G_REGEX_DOTALL | G_REGEX_EXTENDED | G_REGEX_RAW, 0, &err);
         g_assert_no_error(err);
     }
 

commit 5504ec2d73616470d741acee430d526dc1b8d816

Correct regex compile options type if none are passed
compile options are of enum type so don't try to pass pointer instead.
fixes compile failures (GCC13):

log.c:225:46: error: incompatible type for argument 2 of 'g_regex_new'
./common/clib/soup.h:164:59: error: incompatible type for argument 2 of 'g_regex_new

Fixes: e400cda67c81 ("Remove G_REGEX_OPTIMIZE")

common/clib/soup.h
@@ -161,7 +161,7 @@ luaH_soup_parse_uri(lua_State *L)
 static void
 soup_lib_setup_common(void)
 {
-    scheme_reg = g_regex_new("^[a-z][a-z0-9\\+\\-\\.]*:", NULL, 0, NULL);
+    scheme_reg = g_regex_new("^[a-z][a-z0-9\\+\\-\\.]*:", 0, 0, NULL);
 }
 
 #endif

log.c
@@ -222,7 +222,7 @@ va_log(log_level_t lvl, const gchar *fct, const gchar *fmt, va_list ap)
     static GRegex *indent_lines_reg;
     if (!indent_lines_reg) {
         GError *err = NULL;
-        indent_lines_reg = g_regex_new("\n", NULL, 0, &err);
+        indent_lines_reg = g_regex_new("\n", 0, 0, &err);
         g_assert_no_error(err);
     }
     gchar *wrapped = g_regex_replace_literal(indent_lines_reg, msg, -1, 0, "\n" LOG_IND, 0, NULL);