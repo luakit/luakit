bea4ae5988daa23976c73d389e06cbed42d4c22c
c0dev0id <sh+git@codevoid.de>
2026-05-26 18:05:37 +0200

    tests: remove leftover unquoted os.execute calls in run_test.lua
    Thx @balejk for the report

--- tests/run_test.lua
+++ tests/run_test.lua
@@ -131,8 +131,6 @@ local function spawn_luakit_instance(config, ...)
     local cache_dir = util.getenv("XDG_CACHE_HOME") or (util.getenv("HOME") .. "/")
     local gst_dir = cache_dir .. "/gstreamer-1.0"
     if lfs.attributes(gst_dir, "mode") == "directory" then
-        os.execute("mkdir -p " .. env.XDG_CACHE_HOME .. "/gstreamer-1.0/")
-        os.execute("cp "..gst_dir.."/registry.x86_64.bin " .. env.XDG_CACHE_HOME .. "/gstreamer-1.0")
         os.execute(string.format("mkdir -p %q", env.XDG_CACHE_HOME .. "/gstreamer-1.0/"))
         os.execute(string.format("cp %q %q", gst_dir.."/registry.x86_64.bin", env.XDG_CACHE_HOME .. "/gstreamer-1.0"))
     end
